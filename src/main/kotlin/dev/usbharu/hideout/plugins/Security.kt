e44d0b545f806c56adbcac0c00bdd59ef0f9f38d
usbharu <64310155+usbharu@users.noreply.github.com>
2023-03-24 16:07:33 +0900

    fix: セッション認証を廃止

--- src/main/kotlin/dev/usbharu/hideout/plugins/Security.kt
+++ src/main/kotlin/dev/usbharu/hideout/plugins/Security.kt
@@ -1,24 +1,27 @@
 package dev.usbharu.hideout.plugins
 
 import dev.usbharu.hideout.service.IUserAuthService
-import dev.usbharu.hideout.service.UserService
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.sessions.*
-import kotlin.time.Duration.Companion.days
+import kotlin.collections.set
 
 data class UserSession(val username: String) : Principal
 
 const val tokenAuth = "token-auth"
 
 fun Application.configureSecurity(userAuthService: IUserAuthService) {
-    install(Authentication){
+    install(Authentication) {
         bearer(tokenAuth) {
-            authenticate {
-                bearerTokenCredential ->
+            authenticate { bearerTokenCredential ->
                 UserIdPrincipal(bearerTokenCredential.token)
             }
             skipWhen { true }
         }
     }
+//    install(Sessions) {
+//        cookie<UserSession>("MY_SESSION") {
+//            cookie.extensions["SameSite"] = "lax"
+//        }
+//    }
 }
