40f778faa1b7d1625f707f86cc14e2ff50ba7c7f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-03-24 23:50:50 +0900

    feat: とりあえずinbox/outboxでokを返すように

--- src/main/kotlin/dev/usbharu/hideout/routing/userActivityPubRouting.kt
+++ src/main/kotlin/dev/usbharu/hideout/routing/userActivityPubRouting.kt
@@ -1,25 +1,30 @@
 package dev.usbharu.hideout.routing
 
+import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
-fun Application.userActivityPubRouting(){
+fun Application.userActivityPubRouting() {
     routing {
         route("/users/{name}") {
-            route("/inbox"){
+            route("/inbox") {
                 get {
-
+                    call.respond(HttpStatusCode.OK)
                 }
                 post {
+                    call.respond(HttpStatusCode.OK)
 
                 }
             }
             route("/outbox") {
                 get {
+                    call.respond(HttpStatusCode.OK)
 
                 }
                 post {
 
+                    call.respond(HttpStatusCode.OK)
                 }
             }
         }
