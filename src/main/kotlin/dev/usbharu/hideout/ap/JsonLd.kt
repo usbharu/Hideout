e97a3e9e9629e199dac041a732e4442c6481da2f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-03-30 19:36:19 +0900

    feat: でシリアライザーを変更

--- src/main/kotlin/dev/usbharu/hideout/ap/JsonLd.kt
+++ src/main/kotlin/dev/usbharu/hideout/ap/JsonLd.kt
@@ -3,10 +3,14 @@ package dev.usbharu.hideout.ap
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.core.TreeNode
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 open class JsonLd {
     @JsonProperty("@context")
+    @JsonDeserialize(using = ContextDeserializer::class)
     var context:List<String> = emptyList()
 
     @JsonCreator
@@ -15,4 +19,10 @@ open class JsonLd {
     }
 
     protected constructor()
+}
+
+public class ContextDeserializer : JsonDeserializer<List<String>>() {
+    override fun deserialize(p0: com.fasterxml.jackson.core.JsonParser?, p1: com.fasterxml.jackson.databind.DeserializationContext?): List<String> {
+        return emptyList()
+    }
 }
