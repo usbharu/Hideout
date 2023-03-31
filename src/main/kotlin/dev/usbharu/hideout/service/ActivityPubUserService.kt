585c02e824cb5ac317e450db6b51842361168878
usbharu <64310155+usbharu@users.noreply.github.com>
2023-03-31 13:04:51 +0900

    fix: keyidを修正2

--- src/main/kotlin/dev/usbharu/hideout/service/ActivityPubUserService.kt
+++ src/main/kotlin/dev/usbharu/hideout/service/ActivityPubUserService.kt
@@ -45,7 +45,7 @@ class ActivityPubUserService(
         val person = webFingerService.fetchUserModel(actor) ?: throw IllegalArgumentException("actor is not found")
         val inboxUrl = person.inbox ?: throw IllegalArgumentException("inbox is not found")
         httpClient.postAp(
-            inboxUrl, person.preferredUsername!!, Accept(
+            inboxUrl, follow.`object`!!, Accept(
                 name = "Follow",
                 `object` = follow,
                 actor = follow.`object`.orEmpty()
