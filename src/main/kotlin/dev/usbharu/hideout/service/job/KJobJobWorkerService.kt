07500651ae054939e15338e523e132d761651b2f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-05 16:59:16 +0900

    feat: KJobを導入

---
+++ src/main/kotlin/dev/usbharu/hideout/service/job/KJobJobWorkerService.kt
@@ -0,0 +1,28 @@
+package dev.usbharu.hideout.service.job
+
+import dev.usbharu.kjob.exposed.ExposedKJob
+import kjob.core.Job
+import kjob.core.dsl.JobContextWithProps
+import kjob.core.dsl.JobRegisterContext
+import kjob.core.dsl.KJobFunctions
+import kjob.core.kjob
+import org.jetbrains.exposed.sql.Database
+
+class KJobJobWorkerService(private val database: Database) : JobWorkerService {
+
+    val kjob by lazy {
+        kjob(ExposedKJob) {
+            connectionDatabase = database
+            nonBlockingMaxJobs = 10
+            blockingMaxJobs = 10
+            jobExecutionPeriodInSeconds = 10
+        }.start()
+    }
+
+    override fun init(defines: List<Pair<Job,JobRegisterContext<Job, JobContextWithProps<Job>>.(Job) -> KJobFunctions<Job, JobContextWithProps<Job>>>>) {
+        defines.forEach { job ->
+            kjob.register(job.first, job.second)
+        }
+    }
+
+}
