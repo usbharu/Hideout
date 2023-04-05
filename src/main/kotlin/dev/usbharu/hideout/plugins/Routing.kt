217f8cde076c0602d9245b308b5534d35cb6b759
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-05 21:47:29 +0900

    feat: ActivityPub関係のルーティングを整備

--- src/main/kotlin/dev/usbharu/hideout/plugins/Routing.kt
+++ src/main/kotlin/dev/usbharu/hideout/plugins/Routing.kt
@@ -1,15 +1,19 @@
 package dev.usbharu.hideout.plugins
 
+import dev.usbharu.hideout.routing.activitypub.inbox
+import dev.usbharu.hideout.routing.activitypub.outbox
+import dev.usbharu.hideout.routing.activitypub.usersAP
+import dev.usbharu.hideout.service.activitypub.ActivityPubService
+import dev.usbharu.hideout.service.signature.HttpSignatureVerifyService
 import io.ktor.server.routing.*
-import io.ktor.server.response.*
 import io.ktor.server.plugins.autohead.*
 import io.ktor.server.application.*
 
-fun Application.configureRouting() {
+fun Application.configureRouting(httpSignatureVerifyService: HttpSignatureVerifyService,activityPubService: ActivityPubService) {
     install(AutoHeadResponse)
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
+        inbox(httpSignatureVerifyService,activityPubService)
+        outbox()
+        usersAP(activityPubService)
     }
 }
