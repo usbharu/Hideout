88fd245d3f8d180ca5552f5d1ea88d8bf94a32df
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-08 16:39:47 +0900

    feat: ログを詳細に

--- src/main/kotlin/dev/usbharu/hideout/plugins/Monitoring.kt
+++ src/main/kotlin/dev/usbharu/hideout/plugins/Monitoring.kt
@@ -8,6 +8,5 @@ import io.ktor.server.application.*
 fun Application.configureMonitoring() {
     install(CallLogging) {
         level = Level.INFO
-        filter { call -> call.request.path().startsWith("/") }
     }
 }
