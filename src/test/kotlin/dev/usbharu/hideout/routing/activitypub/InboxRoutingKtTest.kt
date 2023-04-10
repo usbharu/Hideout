c5566253fa877600c74949ae0df6f62a461cf5f4
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-10 14:20:41 +0900

    test: sharedInboxのテストを追加

--- src/test/kotlin/dev/usbharu/hideout/routing/activitypub/InboxRoutingKtTest.kt
+++ src/test/kotlin/dev/usbharu/hideout/routing/activitypub/InboxRoutingKtTest.kt
@@ -1,4 +1,33 @@
 package dev.usbharu.hideout.routing.activitypub
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.Nulls
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import dev.usbharu.hideout.plugins.configureRouting
+import dev.usbharu.hideout.plugins.configureSerialization
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.config.*
+import io.ktor.server.testing.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+
 class InboxRoutingKtTest {
-  
+    @Test
+    fun `sharedInboxにGETしたら501が帰ってくる`() = testApplication {
+        environment {
+            config = ApplicationConfig("empty.conf")
+        }
+        application {
+            configureSerialization()
+            configureRouting(mock(), mock(), mock(), mock())
+        }
+        client.get("/inbox").let {
+            Assertions.assertEquals(HttpStatusCode.MethodNotAllowed, it.status)
+        }
+    }
 }
