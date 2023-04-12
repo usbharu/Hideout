71ad3c9c7d895242e203193d002a2f99cb01825c
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-12 15:46:45 +0900

    test: UserRepositoryのテストを追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
 
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation ("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.6.4")
 
     implementation("io.ktor:ktor-client-core:$ktor_version")
     implementation("io.ktor:ktor-client-cio:$ktor_version")
