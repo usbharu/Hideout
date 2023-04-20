5bc49adba84d05a0b9397c3d2c43619b2e715e37
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-21 00:10:17 +0900

    feat: 投稿をinboxに送りつける処理を追加

--- src/main/kotlin/dev/usbharu/hideout/repository/IUserKeyRepository.kt
+++
@@ -1,3 +0,0 @@
-package dev.usbharu.hideout.repository
-
-interface IUserKeyRepository
---
+++ src/main/kotlin/dev/usbharu/hideout/service/activitypub/ActivityPubNoteService.kt
@@ -0,0 +1,12 @@
+package dev.usbharu.hideout.service.activitypub
+
+import dev.usbharu.hideout.domain.model.PostEntity
+import dev.usbharu.hideout.domain.model.ap.Note
+import dev.usbharu.hideout.domain.model.job.DeliverPostJob
+import kjob.core.job.JobProps
+
+interface ActivityPubNoteService {
+
+    suspend fun createNote(post:PostEntity)
+    suspend fun createNoteJob(props:JobProps<DeliverPostJob>)
+}
