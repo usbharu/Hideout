dcbfe96f0d83215ed03add34a95f9c9e96a2f287
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-21 21:43:20 +0900

    fix: 間違えてNoteを直接送りつけていたのでCreateを送りつけるように

---
+++ src/main/kotlin/dev/usbharu/hideout/domain/model/ap/Create.kt
@@ -0,0 +1,30 @@
+package dev.usbharu.hideout.domain.model.ap
+
+open class Create : Object {
+    var `object` : Object? = null
+
+    protected constructor() : super()
+    constructor(type: List<String> = emptyList(), name: String, `object`: Object?) : super(add(type,"Create"), name) {
+        this.`object` = `object`
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is Create) return false
+        if (!super.equals(other)) return false
+
+        return `object` == other.`object`
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + (`object`?.hashCode() ?: 0)
+        return result
+    }
+
+    override fun toString(): String {
+        return "Create(`object`=$`object`) ${super.toString()}"
+    }
+
+
+}
