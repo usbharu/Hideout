02d262f36c2773b43ae81e3ed0502b82e9f876c2
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-21 21:05:26 +0900

    feat: ログを追加

--- src/main/kotlin/dev/usbharu/hideout/service/impl/PostService.kt
+++ src/main/kotlin/dev/usbharu/hideout/service/impl/PostService.kt
@@ -5,9 +5,13 @@ import dev.usbharu.hideout.repository.IPostRepository
 import dev.usbharu.hideout.service.IPostService
 import dev.usbharu.hideout.service.activitypub.ActivityPubNoteService
 import dev.usbharu.hideout.service.job.JobQueueParentService
+import org.slf4j.LoggerFactory
 
 class PostService(private val postRepository:IPostRepository,private val activityPubNoteService: ActivityPubNoteService) : IPostService {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
     override suspend fun create(post: Post) {
+        logger.debug("create post={}",post)
         val postEntity = postRepository.insert(post)
         activityPubNoteService.createNote(postEntity)
     }
