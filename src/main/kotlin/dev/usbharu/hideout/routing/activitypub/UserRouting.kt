4ac23c225a492dd078455fceed6faf948099153b
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-29 13:02:38 +0900

    fix: 軽微な修正、ロガーを追加

--- src/main/kotlin/dev/usbharu/hideout/routing/activitypub/UserRouting.kt
+++ src/main/kotlin/dev/usbharu/hideout/routing/activitypub/UserRouting.kt
@@ -31,10 +31,11 @@ fun Routing.usersAP(activityPubUserService: ActivityPubUserService, userService:
 }
 
 class ContentTypeRouteSelector(private vararg val contentType: ContentType) : RouteSelector() {
-    override fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation {
+        override fun evaluate(context: RoutingResolveContext, segmentIndex: Int): RouteSelectorEvaluation {
 
         val requestContentType =
             ContentType.parse(context.call.request.accept() ?: return RouteSelectorEvaluation.FailedParameter)
+        context.call.application.log.debug("Content-Type: {}", contentType)
         return if (contentType.any { contentType -> contentType.match(requestContentType) }) {
             RouteSelectorEvaluation.Constant
         } else {
