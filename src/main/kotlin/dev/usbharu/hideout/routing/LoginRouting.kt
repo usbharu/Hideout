aad86a31a56efc6a151dc751a9cfe1b1b0ed854b
usbharu <64310155+usbharu@users.noreply.github.com>
2023-04-30 17:05:10 +0900

    feat: サンプルの値を使ってJWTでログイントークンを発行できるように

---
+++ src/main/kotlin/dev/usbharu/hideout/routing/LoginRouting.kt
@@ -0,0 +1,8 @@
+package dev.usbharu.hideout.routing
+
+import dev.usbharu.hideout.service.IUserAuthService
+import io.ktor.server.routing.*
+
+fun Routing.login(userAuthService: IUserAuthService){
+
+}
