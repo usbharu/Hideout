bf6fe19ede4bda0b8e640a4a067631cd10034ccc
usbharu <64310155+usbharu@users.noreply.github.com>
2023-05-01 09:12:15 +0900

    feat: 認証の確認の実装を追加

--- src/main/kotlin/dev/usbharu/hideout/plugins/HTTP.kt
+++ src/main/kotlin/dev/usbharu/hideout/plugins/HTTP.kt
@@ -1,21 +1,20 @@
 package dev.usbharu.hideout.plugins
 
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.plugins.defaultheaders.*
 import io.ktor.server.plugins.forwardedheaders.*
 
 fun Application.configureHTTP() {
-    install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
-        allowHeader("MyCustomHeader")
-        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
-    }
+//    install(CORS) {
+//        allowMethod(HttpMethod.Options)
+//        allowMethod(HttpMethod.Put)
+//        allowMethod(HttpMethod.Delete)
+//        allowMethod(HttpMethod.Patch)
+//        allowHeader(HttpHeaders.Authorization)
+//        allow
+//        allowHeader("MyCustomHeader")
+//        anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
+//    }
     install(DefaultHeaders) {
         header("X-Engine", "Ktor") // will send this header with each response
     }
