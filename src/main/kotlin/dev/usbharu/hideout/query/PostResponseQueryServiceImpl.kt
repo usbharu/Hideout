cff8640b86d9b19da59c3481068855eef142fa28
usbharu <64310155+usbharu@users.noreply.github.com>
2023-08-10 20:05:40 +0900

    refactor: 不要なコードを削除

--- src/main/kotlin/dev/usbharu/hideout/query/PostResponseQueryServiceImpl.kt
+++ src/main/kotlin/dev/usbharu/hideout/query/PostResponseQueryServiceImpl.kt
@@ -5,6 +5,7 @@ import dev.usbharu.hideout.repository.Posts
 import dev.usbharu.hideout.repository.Users
 import dev.usbharu.hideout.repository.toPost
 import dev.usbharu.hideout.repository.toUser
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.innerJoin
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
@@ -12,7 +13,7 @@ import org.koin.core.annotation.Single
 
 @Single
 class PostResponseQueryServiceImpl : PostResponseQueryService {
-    override suspend fun findById(id: Long, userId: Long): PostResponse {
+    override suspend fun findById(id: Long, userId: Long?): PostResponse {
         return Posts
             .innerJoin(Users, onColumn = { Posts.userId }, otherColumn = { Users.id })
             .select { Posts.id eq id }
@@ -21,12 +22,12 @@ class PostResponseQueryServiceImpl : PostResponseQueryService {
     }
 
     override suspend fun findAll(
-        since: Long,
-        until: Long,
-        minId: Long,
-        maxId: Long,
-        limit: Long,
-        userId: Long
+        since: Long?,
+        until: Long?,
+        minId: Long?,
+        maxId: Long?,
+        limit: Int?,
+        userId: Long?
     ): List<PostResponse> {
         return Posts
             .innerJoin(Users, onColumn = { Posts.userId }, otherColumn = { id })
@@ -36,16 +37,32 @@ class PostResponseQueryServiceImpl : PostResponseQueryService {
 
     override suspend fun findByUserId(
         userId: Long,
-        since: Long,
-        until: Long,
-        minId: Long,
-        maxId: Long,
-        limit: Long,
-        userId2: Long
+        since: Long?,
+        until: Long?,
+        minId: Long?,
+        maxId: Long?,
+        limit: Int?,
+        userId2: Long?
     ): List<PostResponse> {
         return Posts
             .innerJoin(Users, onColumn = { Posts.userId }, otherColumn = { id })
             .select { Posts.userId eq userId }
+            .map { PostResponse.from(it.toPost(), it.toUser()) }
+    }
+
+    override suspend fun findByUserNameAndUserDomain(
+        name: String,
+        domain: String,
+        since: Long?,
+        until: Long?,
+        minId: Long?,
+        maxId: Long?,
+        limit: Int?,
+        userId: Long?
+    ): List<PostResponse> {
+        return Posts
+            .innerJoin(Users, onColumn = { Posts.userId }, otherColumn = { id })
+            .select { Users.name eq name and (Users.domain eq domain) }
             .map { PostResponse.from(it.toPost(), it.toUser()) }
     }
 }
