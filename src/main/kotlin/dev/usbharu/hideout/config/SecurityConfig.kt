6aaf01a026e1c5523aaf0da31cead6acadc171d0
usbharu <64310155+usbharu@users.noreply.github.com>
2023-08-28 21:05:34 +0900

    feat: Spring Securityの導入

---
+++ src/main/kotlin/dev/usbharu/hideout/config/SecurityConfig.kt
@@ -0,0 +1,112 @@
+package dev.usbharu.hideout.config
+
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import com.nimbusds.jose.jwk.source.JWKSource
+import com.nimbusds.jose.proc.SecurityContext
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.annotation.Order
+import org.springframework.http.MediaType
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration
+import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher
+import java.security.KeyPairGenerator
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.util.*
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    fun oauth2SecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http)
+        http
+            .exceptionHandling {
+                it.defaultAuthenticationEntryPointFor(
+                    LoginUrlAuthenticationEntryPoint("/login"), MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+                )
+            }
+            .oauth2ResourceServer {
+                it.jwt(Customizer.withDefaults())
+            }
+        return http.build()
+    }
+
+
+    @Bean
+    @Order(2)
+    fun defaultSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }
+            .formLogin(Customizer.withDefaults())
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun genJwkSource(): JWKSource<SecurityContext> {
+        val keyPairGenerator = KeyPairGenerator.getInstance("RSA")
+        keyPairGenerator.initialize(2048)
+        val generateKeyPair = keyPairGenerator.generateKeyPair()
+        val rsaPublicKey = generateKeyPair.public as RSAPublicKey
+        val rsaPrivateKey = generateKeyPair.private as RSAPrivateKey
+        val rsaKey = RSAKey
+            .Builder(rsaPublicKey)
+            .privateKey(rsaPrivateKey)
+            .keyID(UUID.randomUUID().toString())
+            .build()
+
+        val jwkSet = JWKSet(rsaKey)
+        return ImmutableJWKSet(jwkSet)
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = ["hideout.security.jwt.generate"], havingValue = "")
+    fun loadJwkSource(jwkConfig: JwkConfig): JWKSource<SecurityContext> {
+        val rsaKey = RSAKey.Builder(jwkConfig.publicKey)
+            .privateKey(jwkConfig.privateKey)
+            .keyID(jwkConfig.keyId)
+            .build()
+        return ImmutableJWKSet(JWKSet(rsaKey))
+    }
+
+    @Bean
+    fun jwtDecoder(jwkSource: JWKSource<SecurityContext>): JwtDecoder {
+        return OAuth2AuthorizationServerConfiguration.jwtDecoder(jwkSource)
+    }
+
+    @Bean
+    fun authorizationServerSettings(): AuthorizationServerSettings {
+        return AuthorizationServerSettings.builder().build()
+    }
+}
+
+
+@ConfigurationProperties("hideout.security.jwt")
+@ConditionalOnProperty(name = ["hideout.security.jwt.generate"], havingValue = "")
+data class JwkConfig(
+    val keyId: String,
+    val publicKey: RSAPublicKey,
+    val privateKey: RSAPrivateKey
+)
---
+++ src/main/kotlin/dev/usbharu/hideout/util/JsonUtil.kt
@@ -0,0 +1,16 @@
+package dev.usbharu.hideout.util
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+object JsonUtil {
+    val objectMapper = jacksonObjectMapper()
+
+    fun mapToJson(map: Map<*, *>, objectMapper: ObjectMapper = this.objectMapper): String =
+        objectMapper.writeValueAsString(map)
+
+    fun <K, V> jsonToMap(json: String, objectMapper: ObjectMapper = this.objectMapper): Map<K, V> =
+        objectMapper.readValue(json)
+
+}
