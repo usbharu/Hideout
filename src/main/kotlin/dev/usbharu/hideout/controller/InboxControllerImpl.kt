afb606c9e26e536499396c0d49e5e84d73f1a640
usbharu <64310155+usbharu@users.noreply.github.com>
2023-09-19 12:49:39 +0900

    feat: Spring MVCのControllerでActivityPubのエンドポイントを実装

---
+++ src/main/kotlin/dev/usbharu/hideout/controller/InboxControllerImpl.kt
@@ -0,0 +1,15 @@
+package dev.usbharu.hideout.controller
+
+import dev.usbharu.hideout.service.ap.APService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class InboxControllerImpl(private val apService: APService) : InboxController {
+    override suspend fun inbox(string: String): ResponseEntity<Unit> {
+        val parseActivity = apService.parseActivity(string)
+        apService.processActivity(string, parseActivity)
+        return ResponseEntity(HttpStatus.ACCEPTED)
+    }
+}
