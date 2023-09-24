2f4cf69334e376490620acfd1f7f906c38aacb1c
usbharu <64310155+usbharu@users.noreply.github.com>
2023-09-24 10:40:22 +0900

    feat: remove Spring WebFlux

--- src/main/kotlin/dev/usbharu/hideout/controller/mastodon/MastodonStatusesApiContoller.kt
+++ src/main/kotlin/dev/usbharu/hideout/controller/mastodon/MastodonStatusesApiContoller.kt
@@ -5,6 +5,7 @@ import dev.usbharu.hideout.domain.mastodon.model.generated.Status
 import dev.usbharu.hideout.domain.mastodon.model.generated.StatusesRequest
 import dev.usbharu.hideout.domain.model.UserDetailsImpl
 import dev.usbharu.hideout.service.api.mastodon.StatusesApiService
+import kotlinx.coroutines.runBlocking
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.context.SecurityContextHolder
@@ -12,9 +13,9 @@ import org.springframework.stereotype.Controller
 
 @Controller
 class MastodonStatusesApiContoller(private val statusesApiService: StatusesApiService) : StatusApi {
-    override suspend fun apiV1StatusesPost(statusesRequest: StatusesRequest): ResponseEntity<Status> {
+    override fun apiV1StatusesPost(statusesRequest: StatusesRequest): ResponseEntity<Status> = runBlocking {
         val principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal()
         require(principal is UserDetailsImpl)
-        return ResponseEntity(statusesApiService.postStatus(statusesRequest, principal), HttpStatus.OK)
+        ResponseEntity(statusesApiService.postStatus(statusesRequest, principal), HttpStatus.OK)
     }
 }
