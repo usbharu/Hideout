be11bff4cb56cbc8bc3f179e9fa39e6e593559c4
usbharu <64310155+usbharu@users.noreply.github.com>
2023-09-27 14:40:14 +0900

    feat: KJobがMongoDBでも動くように

---
+++ src/main/kotlin/dev/usbharu/hideout/service/job/KjobMongoJobQueueParentService.kt
@@ -0,0 +1,27 @@
+package dev.usbharu.hideout.service.job
+
+import kjob.core.Job
+import kjob.core.dsl.ScheduleContext
+import kjob.core.kjob
+import kjob.mongo.Mongo
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.stereotype.Service
+
+@Service
+@ConditionalOnProperty(name = ["hideout.job-queue.type"], havingValue = "nosql")
+class KjobMongoJobQueueParentService : JobQueueParentService {
+    override fun init(jobDefines: List<Job>) = Unit
+
+    private val kjob = kjob(Mongo) {
+        connectionString = "mongodb://localhost"
+        databaseName = "kjob"
+        jobCollection = "kjob-jobs"
+        lockCollection = "kjob-locks"
+        expireLockInMinutes = 5L
+        isWorker = false
+    }.start()
+
+    override suspend fun <J : Job> schedule(job: J, block: ScheduleContext<J>.(J) -> Unit) {
+        kjob.schedule(job, block)
+    }
+}
