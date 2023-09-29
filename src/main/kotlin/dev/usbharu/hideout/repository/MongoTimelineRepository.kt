5b89c681b0efc79a33e5f3732ba44e43f04c57a2
usbharu <64310155+usbharu@users.noreply.github.com>
2023-09-29 15:44:58 +0900

    feat: TimelineServiceを作成

--- src/main/kotlin/dev/usbharu/hideout/repository/MongoTimelineRepository.kt
+++ src/main/kotlin/dev/usbharu/hideout/repository/MongoTimelineRepository.kt
@@ -3,7 +3,9 @@ package dev.usbharu.hideout.repository
 import dev.usbharu.hideout.domain.model.hideout.entity.Timeline
 import org.springframework.data.mongodb.repository.MongoRepository
 
-interface MongoTimelineRepository : TimelineRepository, MongoRepository<Timeline, Long> {
-    override fun findByUserId(id: Long): List<Timeline>
-    override fun findByUserIdAndTimelineId(userId: Long, timelineId: Long): List<Timeline>
+interface MongoTimelineRepository : MongoRepository<Timeline, Long> {
+
+
+    fun findByUserId(id: Long): List<Timeline>
+    fun findByUserIdAndTimelineId(userId: Long, timelineId: Long): List<Timeline>
 }
---
+++ src/main/kotlin/dev/usbharu/hideout/repository/MongoTimelineRepositoryWrapper.kt
@@ -0,0 +1,38 @@
+package dev.usbharu.hideout.repository
+
+import dev.usbharu.hideout.domain.model.hideout.entity.Timeline
+import dev.usbharu.hideout.service.core.IdGenerateService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import org.springframework.stereotype.Repository
+
+@Repository
+class MongoTimelineRepositoryWrapper(
+    private val mongoTimelineRepository: MongoTimelineRepository,
+    private val idGenerateService: IdGenerateService
+) :
+    TimelineRepository {
+    override suspend fun generateId(): Long = idGenerateService.generateId()
+
+    override suspend fun save(timeline: Timeline): Timeline {
+        return withContext(Dispatchers.IO) {
+            mongoTimelineRepository.save(timeline)
+        }
+    }
+
+    override suspend fun saveAll(timelines: List<Timeline>): List<Timeline> {
+        return mongoTimelineRepository.saveAll(timelines)
+    }
+
+    override suspend fun findByUserId(id: Long): List<Timeline> {
+        return withContext(Dispatchers.IO) {
+            mongoTimelineRepository.findByUserId(id)
+        }
+    }
+
+    override suspend fun findByUserIdAndTimelineId(userId: Long, timelineId: Long): List<Timeline> {
+        return withContext(Dispatchers.IO) {
+            mongoTimelineRepository.findByUserIdAndTimelineId(userId, timelineId)
+        }
+    }
+}
