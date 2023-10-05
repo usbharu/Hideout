c7568a156768f6ad123fcf17d1445d19c252bebb
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-05 11:58:37 +0900

    feat: メディアのアップロード処理の結果を返却するように

--- src/main/kotlin/dev/usbharu/hideout/service/api/mastodon/MediaApiServiceImpl.kt
+++ src/main/kotlin/dev/usbharu/hideout/service/api/mastodon/MediaApiServiceImpl.kt
@@ -1,6 +1,7 @@
 package dev.usbharu.hideout.service.api.mastodon
 
 import dev.usbharu.hideout.domain.mastodon.model.generated.MediaAttachment
+import dev.usbharu.hideout.domain.model.hideout.dto.FileType
 import dev.usbharu.hideout.domain.model.hideout.form.Media
 import dev.usbharu.hideout.service.core.Transaction
 import dev.usbharu.hideout.service.media.MediaService
@@ -14,8 +15,21 @@ class MediaApiServiceImpl(private val mediaService: MediaService, private val tr
         return transaction.transaction {
 
             val uploadLocalMedia = mediaService.uploadLocalMedia(media)
+            val type = when (uploadLocalMedia.type) {
+                FileType.Image -> MediaAttachment.Type.image
+                FileType.Video -> MediaAttachment.Type.video
+                FileType.Audio -> MediaAttachment.Type.audio
+                FileType.Unknown -> MediaAttachment.Type.unknown
+            }
             return@transaction MediaAttachment(
-
+                uploadLocalMedia.id.toString(),
+                type,
+                uploadLocalMedia.url,
+                uploadLocalMedia.thumbnailUrl,
+                null,
+                media.description,
+                uploadLocalMedia.blurHash,
+                uploadLocalMedia.url
             )
         }
     }
