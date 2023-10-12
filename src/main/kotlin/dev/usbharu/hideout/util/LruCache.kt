6aabc7e98d9499e0fee5b804a9b92f64b8a6f8ad
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-12 17:53:02 +0900

    feat: メモリリークの原因となるキャッシュの開放忘れを修正

---
+++ src/main/kotlin/dev/usbharu/hideout/util/LruCache.kt
@@ -0,0 +1,14 @@
+package dev.usbharu.hideout.util
+
+import java.io.Serial
+
+class LruCache<K, V>(private val maxSize: Int) : LinkedHashMap<K, V>(15, 0.75f, true) {
+
+    override fun removeEldestEntry(eldest: MutableMap.MutableEntry<K, V>?): Boolean = size > maxSize
+
+    companion object {
+        @Serial
+        private const val serialVersionUID: Long = -6446947260925053191L
+    }
+
+}
