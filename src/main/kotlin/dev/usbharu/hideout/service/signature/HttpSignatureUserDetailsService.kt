096daed1f8e7257ab6ea1401ccfe63acb840a9d7
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-20 11:05:02 +0900

    feat: HttpSignatureで署名を検証する実装を追加

---
+++ src/main/kotlin/dev/usbharu/hideout/service/signature/HttpSignatureUserDetailsService.kt
@@ -0,0 +1,54 @@
+package dev.usbharu.hideout.service.signature
+
+import dev.usbharu.hideout.exception.FailedToGetResourcesException
+import dev.usbharu.hideout.exception.HttpSignatureVerifyException
+import dev.usbharu.hideout.query.UserQueryService
+import dev.usbharu.hideout.util.RsaUtil
+import dev.usbharu.httpsignature.common.HttpRequest
+import dev.usbharu.httpsignature.common.PublicKey
+import dev.usbharu.httpsignature.verify.FailedVerification
+import dev.usbharu.httpsignature.verify.HttpSignatureVerifier
+import kotlinx.coroutines.runBlocking
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
+
+class HttpSignatureUserDetailsService(
+    private val userQueryService: UserQueryService,
+    private val httpSignatureVerifier: HttpSignatureVerifier
+) :
+    AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
+    override fun loadUserDetails(token: PreAuthenticatedAuthenticationToken): UserDetails = runBlocking {
+        if (token.principal !is String) {
+            throw IllegalStateException("Token is not String")
+        }
+        if (token.credentials !is HttpRequest) {
+            throw IllegalStateException("Credentials is not HttpRequest")
+        }
+
+        val keyId = token.principal as String
+        val findByKeyId = try {
+            userQueryService.findByKeyId(keyId)
+        } catch (e: FailedToGetResourcesException) {
+            throw UsernameNotFoundException("User not found", e)
+        }
+
+        val verify = httpSignatureVerifier.verify(
+            token.credentials as HttpRequest,
+            PublicKey(RsaUtil.decodeRsaPublicKeyPem(findByKeyId.publicKey), keyId)
+        )
+
+        if (verify is FailedVerification) {
+            throw HttpSignatureVerifyException(verify.reason)
+        }
+
+        HttpSignatureUser(
+            username = findByKeyId.name,
+            domain = findByKeyId.domain,
+            credentialsNonExpired = true,
+            accountNonLocked = true,
+            authorities = mutableListOf()
+        )
+    }
+}
