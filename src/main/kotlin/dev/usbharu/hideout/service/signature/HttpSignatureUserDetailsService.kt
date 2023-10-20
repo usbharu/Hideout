5607fe802e5015a70dcf254d015107519212c6e2
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-20 11:55:42 +0900

    feat: ActivityPub関係のリクエストは署名を検証するように

--- src/main/kotlin/dev/usbharu/hideout/service/signature/HttpSignatureUserDetailsService.kt
+++ src/main/kotlin/dev/usbharu/hideout/service/signature/HttpSignatureUserDetailsService.kt
@@ -3,6 +3,7 @@ package dev.usbharu.hideout.service.signature
 import dev.usbharu.hideout.exception.FailedToGetResourcesException
 import dev.usbharu.hideout.exception.HttpSignatureVerifyException
 import dev.usbharu.hideout.query.UserQueryService
+import dev.usbharu.hideout.service.core.Transaction
 import dev.usbharu.hideout.util.RsaUtil
 import dev.usbharu.httpsignature.common.HttpRequest
 import dev.usbharu.httpsignature.common.PublicKey
@@ -16,39 +17,44 @@ import org.springframework.security.web.authentication.preauth.PreAuthenticatedA
 
 class HttpSignatureUserDetailsService(
     private val userQueryService: UserQueryService,
-    private val httpSignatureVerifier: HttpSignatureVerifier
+    private val httpSignatureVerifier: HttpSignatureVerifier,
+    private val transaction: Transaction
 ) :
     AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> {
     override fun loadUserDetails(token: PreAuthenticatedAuthenticationToken): UserDetails = runBlocking {
-        if (token.principal !is String) {
-            throw IllegalStateException("Token is not String")
-        }
-        if (token.credentials !is HttpRequest) {
-            throw IllegalStateException("Credentials is not HttpRequest")
-        }
+        transaction.transaction {
 
-        val keyId = token.principal as String
-        val findByKeyId = try {
-            userQueryService.findByKeyId(keyId)
-        } catch (e: FailedToGetResourcesException) {
-            throw UsernameNotFoundException("User not found", e)
+
+            if (token.principal !is String) {
+                throw IllegalStateException("Token is not String")
+            }
+            if (token.credentials !is HttpRequest) {
+                throw IllegalStateException("Credentials is not HttpRequest")
+            }
+
+            val keyId = token.principal as String
+            val findByKeyId = try {
+                userQueryService.findByKeyId(keyId)
+            } catch (e: FailedToGetResourcesException) {
+                throw UsernameNotFoundException("User not found", e)
+            }
+
+            val verify = httpSignatureVerifier.verify(
+                token.credentials as HttpRequest,
+                PublicKey(RsaUtil.decodeRsaPublicKeyPem(findByKeyId.publicKey), keyId)
+            )
+
+            if (verify is FailedVerification) {
+                throw HttpSignatureVerifyException(verify.reason)
+            }
+
+            HttpSignatureUser(
+                username = findByKeyId.name,
+                domain = findByKeyId.domain,
+                credentialsNonExpired = true,
+                accountNonLocked = true,
+                authorities = mutableListOf()
+            )
         }
-
-        val verify = httpSignatureVerifier.verify(
-            token.credentials as HttpRequest,
-            PublicKey(RsaUtil.decodeRsaPublicKeyPem(findByKeyId.publicKey), keyId)
-        )
-
-        if (verify is FailedVerification) {
-            throw HttpSignatureVerifyException(verify.reason)
-        }
-
-        HttpSignatureUser(
-            username = findByKeyId.name,
-            domain = findByKeyId.domain,
-            credentialsNonExpired = true,
-            accountNonLocked = true,
-            authorities = mutableListOf()
-        )
     }
 }
