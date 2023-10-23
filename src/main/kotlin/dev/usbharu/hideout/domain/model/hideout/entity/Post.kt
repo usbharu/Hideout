657936c3d95b3414cf855bdea56028f970ca7be6
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-23 12:17:48 +0900

    feat: Postをビルダーを使って生成するように

--- src/main/kotlin/dev/usbharu/hideout/domain/model/hideout/entity/Post.kt
+++ src/main/kotlin/dev/usbharu/hideout/domain/model/hideout/entity/Post.kt
@@ -1,6 +1,8 @@
 package dev.usbharu.hideout.domain.model.hideout.entity
 
+import dev.usbharu.hideout.config.CharacterLimit
 import dev.usbharu.hideout.config.Config
+import org.springframework.stereotype.Component
 
 data class Post private constructor(
     val id: Long,
@@ -18,6 +20,7 @@ data class Post private constructor(
 ) {
     companion object {
         @Suppress("FunctionMinLength", "LongParameterList")
+        @Deprecated("Static builder is deprecated")
         fun of(
             id: Long,
             userId: Long,
@@ -34,6 +37,64 @@ data class Post private constructor(
         ): Post {
             val characterLimit = Config.configData.characterLimit
 
+            require(id >= 0) { "id must be greater than or equal to 0." }
+
+            require(userId >= 0) { "userId must be greater than or equal to 0." }
+
+            val limitedOverview = if ((overview?.length ?: 0) >= characterLimit.post.overview) {
+                overview?.substring(0, characterLimit.post.overview)
+            } else {
+                overview
+            }
+
+            val limitedText = if (text.length >= characterLimit.post.text) {
+                text.substring(0, characterLimit.post.text)
+            } else {
+                text
+            }
+
+            require(url.isNotBlank()) { "url must contain non-blank characters" }
+            require(url.length <= characterLimit.general.url) {
+                "url must not exceed ${characterLimit.general.url} characters."
+            }
+
+            require((repostId ?: 0) >= 0) { "repostId must be greater then or equal to 0." }
+            require((replyId ?: 0) >= 0) { "replyId must be greater then or equal to 0." }
+
+            return Post(
+                id = id,
+                userId = userId,
+                overview = limitedOverview,
+                text = limitedText,
+                createdAt = createdAt,
+                visibility = visibility,
+                url = url,
+                repostId = repostId,
+                replyId = replyId,
+                sensitive = sensitive,
+                apId = apId,
+                mediaIds = mediaIds
+            )
+        }
+    }
+
+    @Component
+    class PostBuilder(private val characterLimit: CharacterLimit) {
+        @Suppress("FunctionMinLength", "LongParameterList")
+        fun of(
+            id: Long,
+            userId: Long,
+            overview: String? = null,
+            text: String,
+            createdAt: Long,
+            visibility: Visibility,
+            url: String,
+            repostId: Long? = null,
+            replyId: Long? = null,
+            sensitive: Boolean = false,
+            apId: String = url,
+            mediaIds: List<Long> = emptyList()
+        ): Post {
             require(id >= 0) { "id must be greater than or equal to 0." }
 
             require(userId >= 0) { "userId must be greater than or equal to 0." }
