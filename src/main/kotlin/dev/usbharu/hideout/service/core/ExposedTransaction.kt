3d795bbd8aea932479af6ec239efbdea91f6f2e5
usbharu <64310155+usbharu@users.noreply.github.com>
2023-10-24 20:05:53 +0900

    feat: ログのMDCをkotlinx.coroutineに対応

--- src/main/kotlin/dev/usbharu/hideout/service/core/ExposedTransaction.kt
+++ src/main/kotlin/dev/usbharu/hideout/service/core/ExposedTransaction.kt
@@ -1,18 +1,19 @@
 package dev.usbharu.hideout.service.core
 
+import kotlinx.coroutines.slf4j.MDCContext
 import org.jetbrains.exposed.sql.transactions.experimental.newSuspendedTransaction
 import org.springframework.stereotype.Service
 
 @Service
 class ExposedTransaction : Transaction {
     override suspend fun <T> transaction(block: suspend () -> T): T {
-        return newSuspendedTransaction {
+        return newSuspendedTransaction(MDCContext()) {
             block()
         }
     }
 
     override suspend fun <T> transaction(transactionLevel: Int, block: suspend () -> T): T {
-        return newSuspendedTransaction(transactionIsolation = transactionLevel) {
+        return newSuspendedTransaction(MDCContext(), transactionIsolation = transactionLevel) {
             block()
         }
     }
