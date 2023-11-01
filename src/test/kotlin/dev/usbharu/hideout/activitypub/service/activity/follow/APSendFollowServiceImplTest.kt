351e99cbe95e52dc01245d33c57eb4d380fe023f
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-01 13:51:11 +0900

    test: テストのパッケージを修正

---
+++ src/test/kotlin/dev/usbharu/hideout/activitypub/service/activity/follow/APSendFollowServiceImplTest.kt
@@ -0,0 +1,37 @@
+package dev.usbharu.hideout.activitypub.service.activity.follow
+
+import dev.usbharu.hideout.activitypub.domain.model.Follow
+import dev.usbharu.hideout.activitypub.service.common.APRequestService
+import dev.usbharu.hideout.core.service.follow.SendFollowDto
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+import utils.UserBuilder
+
+class APSendFollowServiceImplTest {
+    @Test
+    fun `sendFollow フォローするユーザーのinboxにFollowオブジェクトが送られる`() = runTest {
+        val apRequestService = mock<APRequestService>()
+        val apSendFollowServiceImpl = APSendFollowServiceImpl(apRequestService)
+
+        val sendFollowDto = SendFollowDto(
+            UserBuilder.localUserOf(),
+            UserBuilder.remoteUserOf()
+        )
+        apSendFollowServiceImpl.sendFollow(sendFollowDto)
+
+        val value = Follow(
+            name = "Follow",
+            `object` = sendFollowDto.followTargetUserId.url,
+            actor = sendFollowDto.userId.url
+        )
+        verify(apRequestService, times(1)).apPost(
+            eq(sendFollowDto.followTargetUserId.inbox),
+            eq(value),
+            eq(sendFollowDto.userId)
+        )
+    }
+}
---
+++ src/test/kotlin/dev/usbharu/hideout/application/service/id/TwitterSnowflakeIdGenerateServiceTest.kt
@@ -0,0 +1,31 @@
+package dev.usbharu.hideout.application.service.id
+
+// import kotlinx.coroutines.NonCancellable.message
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class TwitterSnowflakeIdGenerateServiceTest {
+    @Test
+    fun noDuplicateTest() = runBlocking {
+        val mutex = Mutex()
+        val mutableListOf = mutableListOf<Long>()
+        coroutineScope {
+            repeat(500000) {
+                launch(Dispatchers.IO) {
+                    val id = TwitterSnowflakeIdGenerateService.generateId()
+                    mutex.withLock {
+                        mutableListOf.add(id)
+                    }
+                }
+            }
+        }
+
+        assertEquals(0, mutableListOf.size - mutableListOf.toSet().size)
+    }
+}
