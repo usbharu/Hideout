82974b5abeb827b78906a7fafc8a10a9b5ba10c7
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-02 18:25:50 +0900

    feat: リモートのメディアを保存するように

--- src/main/kotlin/dev/usbharu/hideout/core/service/media/MediaService.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/media/MediaService.kt
@@ -1,8 +1,9 @@
 package dev.usbharu.hideout.core.service.media
 
+import dev.usbharu.hideout.core.domain.model.media.Media
 import dev.usbharu.hideout.mastodon.interfaces.api.media.MediaRequest
 
 interface MediaService {
-    suspend fun uploadLocalMedia(mediaRequest: MediaRequest): dev.usbharu.hideout.core.domain.model.media.Media
-    suspend fun uploadRemoteMedia(remoteMedia: RemoteMedia)
+    suspend fun uploadLocalMedia(mediaRequest: MediaRequest): Media
+    suspend fun uploadRemoteMedia(remoteMedia: RemoteMedia): Media
 }
