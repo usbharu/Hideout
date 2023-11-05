b62058c668d6d1c66ec7ed6c23f2cc1dab8a179b
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-05 14:46:20 +0900

    test: noteのエンドポイントのテストを追加

--- src/main/kotlin/dev/usbharu/hideout/activitypub/interfaces/api/note/NoteApControllerImpl.kt
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/interfaces/api/note/NoteApControllerImpl.kt
@@ -4,8 +4,7 @@ import dev.usbharu.hideout.activitypub.domain.model.Note
 import dev.usbharu.hideout.activitypub.service.objects.note.NoteApApiService
 import dev.usbharu.hideout.core.infrastructure.springframework.httpsignature.HttpSignatureUser
 import org.springframework.http.ResponseEntity
-import org.springframework.security.core.annotation.CurrentSecurityContext
-import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -14,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 class NoteApControllerImpl(private val noteApApiService: NoteApApiService) : NoteApController {
     override suspend fun postsAp(
         @PathVariable(value = "postId") postId: Long,
-        @CurrentSecurityContext context: SecurityContext
     ): ResponseEntity<Note> {
+        val context = SecurityContextHolder.getContext()
         val userId =
             if (context.authentication is PreAuthenticatedAuthenticationToken &&
                 context.authentication.details is HttpSignatureUser
