59298f77c2cac1f83c1f22d133ed22d64d682513
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-11 17:37:22 +0900

    test: noteの匿名のときのテストを追加

--- src/main/kotlin/dev/usbharu/hideout/activitypub/service/objects/note/NoteApApiServiceImpl.kt
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/objects/note/NoteApApiServiceImpl.kt
@@ -3,6 +3,7 @@ package dev.usbharu.hideout.activitypub.service.objects.note
 import dev.usbharu.hideout.activitypub.domain.model.Note
 import dev.usbharu.hideout.activitypub.query.NoteQueryService
 import dev.usbharu.hideout.application.external.Transaction
+import dev.usbharu.hideout.core.domain.exception.FailedToGetResourcesException
 import dev.usbharu.hideout.core.domain.model.post.Visibility
 import dev.usbharu.hideout.core.query.FollowerQueryService
 import org.springframework.stereotype.Service
@@ -14,7 +15,11 @@ class NoteApApiServiceImpl(
     private val transaction: Transaction
 ) : NoteApApiService {
     override suspend fun getNote(postId: Long, userId: Long?): Note? = transaction.transaction {
-        val findById = noteQueryService.findById(postId)
+        val findById = try {
+            noteQueryService.findById(postId)
+        } catch (_: FailedToGetResourcesException) {
+            return@transaction null
+        }
         when (findById.second.visibility) {
             Visibility.PUBLIC, Visibility.UNLISTED -> {
                 return@transaction findById.first
