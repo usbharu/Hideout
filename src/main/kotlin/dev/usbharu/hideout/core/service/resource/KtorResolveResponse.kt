48371921396131dddcc3db479352badf0f51d05c
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-18 11:37:51 +0900

    feat: 新規ユーザー作成時にInstanceを取得するように

--- src/main/kotlin/dev/usbharu/hideout/core/service/resource/KtorResolveResponse.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/resource/KtorResolveResponse.kt
@@ -7,7 +7,24 @@ import java.io.InputStream
 
 class KtorResolveResponse(val ktorHttpResponse: HttpResponse) : ResolveResponse {
 
+    private lateinit var _bodyAsText: String
+    private lateinit var _bodyAsBytes: ByteArray
+
     override suspend fun body(): InputStream = ktorHttpResponse.bodyAsChannel().toInputStream()
+    override suspend fun bodyAsText(): String {
+        if (!this::_bodyAsText.isInitialized) {
+            _bodyAsText = ktorHttpResponse.bodyAsText()
+        }
+        return _bodyAsText
+    }
+
+    override suspend fun bodyAsBytes(): ByteArray {
+        if (!this::_bodyAsBytes.isInitialized) {
+            _bodyAsBytes = ktorHttpResponse.readBytes()
+        }
+        return _bodyAsBytes
+    }
+
     override suspend fun header(): Map<String, List<String>> = ktorHttpResponse.headers.toMap()
     override suspend fun status(): Int = ktorHttpResponse.status.value
     override suspend fun statusMessage(): String = ktorHttpResponse.status.description
--- src/main/kotlin/dev/usbharu/hideout/core/service/resource/ResolveResponse.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/resource/ResolveResponse.kt
@@ -4,6 +4,8 @@ import java.io.InputStream
 
 interface ResolveResponse {
     suspend fun body(): InputStream
+    suspend fun bodyAsText(): String
+    suspend fun bodyAsBytes(): ByteArray
     suspend fun header(): Map<String, List<String>>
     suspend fun status(): Int
     suspend fun statusMessage(): String
