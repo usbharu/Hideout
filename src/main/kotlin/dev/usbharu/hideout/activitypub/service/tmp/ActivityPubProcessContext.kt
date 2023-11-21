829929a241096cd9cad3ec198d9e4bdd32d02c0a
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-22 01:57:42 +0900

    feat: InboxJobProcessorを追加

---
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/tmp/ActivityPubProcessContext.kt
@@ -0,0 +1,14 @@
+package dev.usbharu.hideout.activitypub.service.tmp
+
+import com.fasterxml.jackson.databind.JsonNode
+import dev.usbharu.hideout.activitypub.domain.model.objects.Object
+import dev.usbharu.httpsignature.common.HttpRequest
+import dev.usbharu.httpsignature.verify.Signature
+
+data class ActivityPubProcessContext<T : Object>(
+    val activity: T,
+    val jsonNode: JsonNode,
+    val httpRequest: HttpRequest,
+    val signature: Signature?,
+    val isAuthorized: Boolean
+)
