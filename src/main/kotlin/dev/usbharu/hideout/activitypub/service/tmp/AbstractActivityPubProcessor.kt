3009ca153290a5c9d8e8f26f7ed73cf77e6bfa68
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-22 02:14:21 +0900

    feat: Createのアクティビティに対応

--- src/main/kotlin/dev/usbharu/hideout/activitypub/service/tmp/AbstractActivityPubProcessor.kt
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/tmp/AbstractActivityPubProcessor.kt
@@ -2,14 +2,21 @@ package dev.usbharu.hideout.activitypub.service.tmp
 
 import dev.usbharu.hideout.activitypub.domain.exception.ActivityPubProcessException
 import dev.usbharu.hideout.activitypub.domain.exception.FailedProcessException
+import dev.usbharu.hideout.activitypub.domain.exception.HttpSignatureUnauthorizedException
 import dev.usbharu.hideout.activitypub.domain.model.objects.Object
 import dev.usbharu.hideout.application.external.Transaction
 import org.slf4j.LoggerFactory
 
-abstract class AbstractActivityPubProcessor<T : Object>(val transaction: Transaction) : ActivityPubProcessor<T> {
+abstract class AbstractActivityPubProcessor<T : Object>(
+    private val transaction: Transaction,
+    private val allowUnauthorized: Boolean = false
+) : ActivityPubProcessor<T> {
     private val logger = LoggerFactory.getLogger(this::class.java)
 
     override suspend fun process(activity: ActivityPubProcessContext<T>) {
+        if (activity.isAuthorized.not() && allowUnauthorized.not()) {
+            throw HttpSignatureUnauthorizedException()
+        }
         logger.info("START ActivityPub process")
         try {
             transaction.transaction {
