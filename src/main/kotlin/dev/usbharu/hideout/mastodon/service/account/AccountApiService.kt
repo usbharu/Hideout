e2bb2f6c2aff82f5611292bb931ffe20397c0d62
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-21 12:38:06 +0900

    feat: フォローのMastodon互換APIを作成

--- src/main/kotlin/dev/usbharu/hideout/mastodon/service/account/AccountApiService.kt
+++ src/main/kotlin/dev/usbharu/hideout/mastodon/service/account/AccountApiService.kt
@@ -1,25 +1,28 @@
 package dev.usbharu.hideout.mastodon.service.account
 
 import dev.usbharu.hideout.application.external.Transaction
+import dev.usbharu.hideout.core.domain.model.user.UserRepository
+import dev.usbharu.hideout.core.query.FollowerQueryService
 import dev.usbharu.hideout.core.service.user.UserCreateDto
 import dev.usbharu.hideout.core.service.user.UserService
-import dev.usbharu.hideout.domain.mastodon.model.generated.Account
-import dev.usbharu.hideout.domain.mastodon.model.generated.CredentialAccount
-import dev.usbharu.hideout.domain.mastodon.model.generated.CredentialAccountSource
-import dev.usbharu.hideout.domain.mastodon.model.generated.Role
+import dev.usbharu.hideout.domain.mastodon.model.generated.*
 import org.springframework.stereotype.Service
 
 @Service
 interface AccountApiService {
     suspend fun verifyCredentials(userid: Long): CredentialAccount
     suspend fun registerAccount(userCreateDto: UserCreateDto): Unit
+    suspend fun follow(userid: Long, followeeId: Long): Relationship
+    suspend fun account(id: Long): Account
 }
 
 @Service
 class AccountApiServiceImpl(
     private val accountService: AccountService,
     private val transaction: Transaction,
-    private val userService: UserService
+    private val userService: UserService,
+    private val followerQueryService: FollowerQueryService,
+    private val userRepository: UserRepository
 ) :
     AccountApiService {
     override suspend fun verifyCredentials(userid: Long): CredentialAccount = transaction.transaction {
@@ -29,6 +32,42 @@ class AccountApiServiceImpl(
 
     override suspend fun registerAccount(userCreateDto: UserCreateDto) {
         userService.createLocalUser(UserCreateDto(userCreateDto.name, userCreateDto.name, "", userCreateDto.password))
+    }
+
+    override suspend fun follow(userid: Long, followeeId: Long): Relationship = transaction.transaction {
+
+        val alreadyFollow = followerQueryService.alreadyFollow(followeeId, userid)
+
+
+        val followRequest = if (alreadyFollow) {
+            true
+        } else {
+            userService.followRequest(followeeId, userid)
+        }
+
+        val alreadyFollow1 = followerQueryService.alreadyFollow(userid, followeeId)
+
+        val followRequestsById = userRepository.findFollowRequestsById(followeeId, userid)
+
+        return@transaction Relationship(
+            followeeId.toString(),
+            followRequest,
+            true,
+            false,
+            alreadyFollow1,
+            false,
+            false,
+            false,
+            false,
+            followRequestsById,
+            false,
+            false,
+            ""
+        )
+    }
+
+    override suspend fun account(id: Long): Account = transaction.transaction {
+        return@transaction accountService.findById(id)
     }
 
     private fun from(account: Account): CredentialAccount {
