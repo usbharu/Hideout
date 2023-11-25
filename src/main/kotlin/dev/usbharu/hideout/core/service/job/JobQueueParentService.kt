dcac609b9432837fe38270301364d6e3e5345069
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-25 16:46:03 +0900

    feat: 型安全ジョブキューをすべてのジョブキューに適用

--- src/main/kotlin/dev/usbharu/hideout/core/service/job/JobQueueParentService.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/job/JobQueueParentService.kt
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service
 interface JobQueueParentService {
 
     fun init(jobDefines: List<Job>)
+
+    @Deprecated("use type safe → scheduleTypeSafe")
     suspend fun <J : Job> schedule(job: J, block: ScheduleContext<J>.(J) -> Unit = {})
     suspend fun <T, J : HideoutJob<T, J>> scheduleTypeSafe(job: J, jobProps: T)
 }
