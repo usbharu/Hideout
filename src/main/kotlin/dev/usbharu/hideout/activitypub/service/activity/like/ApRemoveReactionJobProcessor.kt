7fb7fe8ff499692bb48a71873ac17631b7cf9c5b
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-27 14:09:44 +0900

    feat: その他のJobProcessorを追加

---
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/activity/like/ApRemoveReactionJobProcessor.kt
@@ -0,0 +1,43 @@
+package dev.usbharu.hideout.activitypub.service.activity.like
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import dev.usbharu.hideout.activitypub.domain.model.Like
+import dev.usbharu.hideout.activitypub.domain.model.Undo
+import dev.usbharu.hideout.activitypub.service.common.APRequestService
+import dev.usbharu.hideout.application.config.ApplicationConfig
+import dev.usbharu.hideout.application.external.Transaction
+import dev.usbharu.hideout.core.external.job.DeliverRemoveReactionJob
+import dev.usbharu.hideout.core.external.job.DeliverRemoveReactionJobParam
+import dev.usbharu.hideout.core.query.UserQueryService
+import dev.usbharu.hideout.core.service.job.JobProcessor
+import java.time.Instant
+
+class ApRemoveReactionJobProcessor(
+    private val userQueryService: UserQueryService,
+    private val transaction: Transaction,
+    private val objectMapper: ObjectMapper,
+    private val apRequestService: APRequestService,
+    private val applicationConfig: ApplicationConfig
+) : JobProcessor<DeliverRemoveReactionJobParam, DeliverRemoveReactionJob> {
+    override suspend fun process(param: DeliverRemoveReactionJobParam): Unit = transaction.transaction {
+
+        val like = objectMapper.readValue<Like>(param.like)
+
+        val signer = userQueryService.findByUrl(param.actor)
+
+        apRequestService.apPost(
+            param.inbox,
+            Undo(
+                name = "Undo Reaction",
+                actor = param.actor,
+                `object` = like,
+                id = "${applicationConfig.url}/undo/like/${param.id}",
+                published = Instant.now()
+            ),
+            signer
+        )
+    }
+
+    override fun job(): DeliverRemoveReactionJob = DeliverRemoveReactionJob
+}
---
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/objects/note/ApNoteJobProcessor.kt
@@ -0,0 +1,40 @@
+package dev.usbharu.hideout.activitypub.service.objects.note
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+import dev.usbharu.hideout.activitypub.domain.model.Create
+import dev.usbharu.hideout.activitypub.service.common.APRequestService
+import dev.usbharu.hideout.application.external.Transaction
+import dev.usbharu.hideout.core.external.job.DeliverPostJob
+import dev.usbharu.hideout.core.external.job.DeliverPostJobParam
+import dev.usbharu.hideout.core.query.UserQueryService
+import dev.usbharu.hideout.core.service.job.JobProcessor
+import org.slf4j.LoggerFactory
+
+class ApNoteJobProcessor(
+    private val transaction: Transaction,
+    private val objectMapper: ObjectMapper,
+    private val userQueryService: UserQueryService,
+    private val apRequestService: APRequestService
+) : JobProcessor<DeliverPostJobParam, DeliverPostJob> {
+    override suspend fun process(param: DeliverPostJobParam) {
+        val create = objectMapper.readValue<Create>(param.create)
+        transaction.transaction {
+            val signer = userQueryService.findByUrl(param.actor)
+
+            logger.debug("CreateNoteJob: actor: {} create: {} inbox: {}", param.actor, create, param.inbox)
+
+            apRequestService.apPost(
+                param.inbox,
+                create,
+                signer
+            )
+        }
+    }
+
+    override fun job(): DeliverPostJob = DeliverPostJob
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(ApNoteJobProcessor::class.java)
+    }
+}
