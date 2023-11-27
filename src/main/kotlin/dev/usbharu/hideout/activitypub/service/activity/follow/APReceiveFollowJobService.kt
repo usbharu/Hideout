640dff53cf6881c1d7fe1617319a0438b23f713c
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-27 14:13:08 +0900

    refactor: 不要になったAPServiceを削除

--- src/main/kotlin/dev/usbharu/hideout/activitypub/service/activity/follow/APReceiveFollowJobService.kt
+++
@@ -1,9 +0,0 @@
-package dev.usbharu.hideout.activitypub.service.activity.follow
-
-import dev.usbharu.hideout.core.external.job.ReceiveFollowJob
-import kjob.core.job.JobProps
-
-@Deprecated("use activitypub processor")
-interface APReceiveFollowJobService {
-    suspend fun receiveFollowJob(props: JobProps<ReceiveFollowJob>)
-}
