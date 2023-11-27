e5d1a8d4a66cb744e1d809792ee5e8779cf60735
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-27 15:52:49 +0900

    style: fix lint

--- src/main/kotlin/dev/usbharu/hideout/core/service/job/JobProcessor.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/job/JobProcessor.kt
@@ -2,7 +2,7 @@ package dev.usbharu.hideout.core.service.job
 
 import dev.usbharu.hideout.core.external.job.HideoutJob
 
-interface JobProcessor<out T, out R : HideoutJob<T, R>> {
+interface JobProcessor<in T, out R : HideoutJob<@UnsafeVariance T, R>> {
     suspend fun process(param: @UnsafeVariance T)
     fun job(): R
 }
