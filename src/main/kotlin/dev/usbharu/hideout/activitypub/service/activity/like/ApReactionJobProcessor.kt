1b721c5a0c660227425a7028b0956fcd3d1069a0
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-27 14:09:44 +0900

    feat: その他のJobProcessorを追加

---
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/service/activity/like/ApReactionJobProcessor.kt
@@ -0,0 +1,35 @@
+package dev.usbharu.hideout.activitypub.service.activity.like
+
+import dev.usbharu.hideout.activitypub.domain.model.Like
+import dev.usbharu.hideout.activitypub.service.common.APRequestService
+import dev.usbharu.hideout.application.config.ApplicationConfig
+import dev.usbharu.hideout.application.external.Transaction
+import dev.usbharu.hideout.core.external.job.DeliverReactionJob
+import dev.usbharu.hideout.core.external.job.DeliverReactionJobParam
+import dev.usbharu.hideout.core.query.UserQueryService
+import dev.usbharu.hideout.core.service.job.JobProcessor
+
+class ApReactionJobProcessor(
+    private val userQueryService: UserQueryService,
+    private val apRequestService: APRequestService,
+    private val applicationConfig: ApplicationConfig,
+    private val transaction: Transaction
+) : JobProcessor<DeliverReactionJobParam, DeliverReactionJob> {
+    override suspend fun process(param: DeliverReactionJobParam): Unit = transaction.transaction {
+        val signer = userQueryService.findByUrl(param.actor)
+
+        apRequestService.apPost(
+            param.inbox,
+            Like(
+                name = "Like",
+                actor = param.actor,
+                `object` = param.postUrl,
+                id = "${applicationConfig.url}/liek/note/${param.id}",
+                content = param.reaction
+            ),
+            signer
+        )
+    }
+
+    override fun job(): DeliverReactionJob = DeliverReactionJob
+}
