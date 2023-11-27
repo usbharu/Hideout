ed0db751a3e72cf5514a713650f4d8e5976a643a
usbharu <64310155+usbharu@users.noreply.github.com>
2023-11-27 13:52:42 +0900

    feat: 不要になったAPServiceを削除

--- src/main/kotlin/dev/usbharu/hideout/activitypub/service/activity/delete/APReceiveDeleteService.kt
+++
@@ -1,7 +0,0 @@
-package dev.usbharu.hideout.activitypub.service.activity.delete
-
-import dev.usbharu.hideout.activitypub.domain.model.Delete
-
-interface APReceiveDeleteService {
-    suspend fun receiveDelete(delete: Delete)
-}
