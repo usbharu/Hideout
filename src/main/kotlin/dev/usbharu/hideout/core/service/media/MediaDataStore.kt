eb6e0622dcc8e2388acc12c6fa6bd9abf36eac8a
usbharu <64310155+usbharu@users.noreply.github.com>
2023-12-06 14:24:36 +0900

    doc: LocalFileSystemMediaDataStore.ktにドキュメントを追加

--- src/main/kotlin/dev/usbharu/hideout/core/service/media/MediaDataStore.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/service/media/MediaDataStore.kt
@@ -1,7 +1,31 @@
 package dev.usbharu.hideout.core.service.media
 
+/**
+ * メディアを保存するインタフェース
+ *
+ */
 interface MediaDataStore {
+    /**
+     * InputStreamを使用してメディアを保存します
+     *
+     * @param dataMediaSave FileとThumbnailのinputStream
+     * @return 保存されたメディア
+     */
     suspend fun save(dataMediaSave: MediaSave): SavedMedia
+
+    /**
+     * 一時ファイルのパスを使用してメディアを保存します
+     *
+     * @param dataSaveRequest FileとThumbnailのパス
+     * @return 保存されたメディア
+     */
     suspend fun save(dataSaveRequest: MediaSaveRequest): SavedMedia
+
+    /**
+     * メディアを削除します
+     * 実装はサムネイル、メタデータなども削除するべきです。
+     *
+     * @param id 削除するメディアのid 通常は[SuccessSavedMedia.name]を指定します。
+     */
     suspend fun delete(id: String)
 }
