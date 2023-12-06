73e04bdd8abfe8c99b1a89747ed1b21f7c2b5d09
usbharu <64310155+usbharu@users.noreply.github.com>
2023-12-06 18:19:33 +0900

    fix: #191 ImageのmediaTypeをnullableに

--- src/main/kotlin/dev/usbharu/hideout/activitypub/domain/model/Image.kt
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/domain/model/Image.kt
@@ -4,12 +4,11 @@ import dev.usbharu.hideout.activitypub.domain.model.objects.Object
 
 open class Image(
     type: List<String> = emptyList(),
-    val mediaType: String,
+    val mediaType: String? = null,
     val url: String
 ) : Object(
     add(type, "Image")
 ) {
-
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
@@ -25,10 +24,18 @@ open class Image(
 
     override fun hashCode(): Int {
         var result = super.hashCode()
-        result = 31 * result + mediaType.hashCode()
+        result = 31 * result + (mediaType?.hashCode() ?: 0)
         result = 31 * result + url.hashCode()
         return result
     }
 
-    override fun toString(): String = "Image(mediaType=$mediaType, url=$url) ${super.toString()}"
+    override fun toString(): String {
+        return "Image(" +
+                "mediaType=$mediaType, " +
+                "url='$url'" +
+                ")" +
+                " ${super.toString()}"
+    }
+
+
 }
