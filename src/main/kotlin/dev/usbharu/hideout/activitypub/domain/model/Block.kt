2ffa880c11279f0838614207963c323bfc5b61e0
usbharu <64310155+usbharu@users.noreply.github.com>
2023-12-07 16:27:55 +0900

    feat: BlockのJSONデシリアライズ用POJOを追加

---
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/domain/model/Block.kt
@@ -0,0 +1,45 @@
+package dev.usbharu.hideout.activitypub.domain.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import dev.usbharu.hideout.activitypub.domain.model.objects.Object
+
+open class Block(
+    override val actor: String,
+    override val id: String,
+    @JsonProperty("object") val apObject: String
+) :
+    Object(listOf("Block")), HasId, HasActor {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as Block
+
+        if (actor != other.actor) return false
+        if (id != other.id) return false
+        if (apObject != other.apObject) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + actor.hashCode()
+        result = 31 * result + id.hashCode()
+        result = 31 * result + apObject.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Block(" +
+                "actor='$actor', " +
+                "id='$id', " +
+                "apObject='$apObject'" +
+                ")" +
+                " ${super.toString()}"
+    }
+
+
+}
