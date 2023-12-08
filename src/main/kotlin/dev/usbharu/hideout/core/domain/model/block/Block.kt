1a7cec5c7bde11216e38d1b2588ee0ecc198f9a7
usbharu <64310155+usbharu@users.noreply.github.com>
2023-12-08 12:59:43 +0900

    doc: ドキュメントを追加

--- src/main/kotlin/dev/usbharu/hideout/core/domain/model/block/Block.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/domain/model/block/Block.kt
@@ -1,5 +1,11 @@
 package dev.usbharu.hideout.core.domain.model.block
 
+/**
+ * ブロック関係を表します
+ *
+ * @property userId ブロックの動作を行ったユーザーid
+ * @property target ブロックの対象のユーザーid
+ */
 data class Block(
     val userId: Long,
     val target: Long
