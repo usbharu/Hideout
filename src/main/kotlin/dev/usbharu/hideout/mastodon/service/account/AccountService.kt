69a889a8d70a68ad0afb3d1a7027ffc73308cbef
usbharu <64310155+usbharu@users.noreply.github.com>
2023-12-12 16:48:25 +0900

    feat: AccountQueryServiceを作成し、アカウントAPIから取得される情報を正確に

--- src/main/kotlin/dev/usbharu/hideout/mastodon/service/account/AccountService.kt
+++ src/main/kotlin/dev/usbharu/hideout/mastodon/service/account/AccountService.kt
@@ -1,9 +1,7 @@
 package dev.usbharu.hideout.mastodon.service.account
 
-import dev.usbharu.hideout.application.config.ApplicationConfig
-import dev.usbharu.hideout.core.domain.model.actor.Actor
-import dev.usbharu.hideout.core.query.ActorQueryService
 import dev.usbharu.hideout.domain.mastodon.model.generated.Account
+import dev.usbharu.hideout.mastodon.query.AccountQueryService
 import org.springframework.stereotype.Service
 
 @Service
@@ -14,41 +12,13 @@ interface AccountService {
 
 @Service
 class AccountServiceImpl(
-    private val actorQueryService: ActorQueryService,
-    private val applicationConfig: ApplicationConfig
+    private val accountQueryService: AccountQueryService
 ) : AccountService {
     override suspend fun findById(id: Long): Account {
-        val findById = actorQueryService.findById(id)
-        return toAccount(findById)
+        return accountQueryService.findById(id)
     }
 
-    private fun toAccount(findById: Actor): Account {
-        val userUrl = applicationConfig.url.toString() + "/users/" + findById.id.toString()
-
-        return Account(
-            id = findById.id.toString(),
-            username = findById.name,
-            acct = "${findById.name}@${findById.domain}",
-            url = findById.url,
-            displayName = findById.screenName,
-            note = findById.description,
-            avatar = "$userUrl/icon.jpg",
-            avatarStatic = "$userUrl/icon.jpg",
-            header = "$userUrl/header.jpg",
-            headerStatic = "$userUrl/header.jpg",
-            locked = findById.locked,
-            fields = emptyList(),
-            emojis = emptyList(),
-            bot = false,
-            group = false,
-            discoverable = false,
-            createdAt = findById.createdAt.toString(),
-            lastStatusAt = findById.createdAt.toString(),
-            statusesCount = 0,
-            followersCount = 0,
-        )
+    override suspend fun findByIds(ids: List<Long>): List<Account> {
+        return accountQueryService.findByIds(ids)
     }
-
-    override suspend fun findByIds(ids: List<Long>): List<Account> =
-        actorQueryService.findByIds(ids).map { toAccount(it) }
 }
