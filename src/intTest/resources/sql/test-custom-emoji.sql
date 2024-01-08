7dcdb118c95a25ecdab3afcd3043a9ad48ea00ca
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-08 17:06:09 +0900

    feat: 絵文字リアクションAPIを修正

---
+++ src/intTest/resources/sql/test-custom-emoji.sql
@@ -0,0 +1,3 @@
+insert into emojis(id, name, domain, instance_id, url, category, created_at)
+VALUES (1, 'kotlin', 'example.com', null, 'https://example.com/emojis/kotlin', null,
+        TIMESTAMP '2024-01-08 16:51:30.036');
