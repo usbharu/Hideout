744b7f6efb340e113f7200c73c795363b2322802
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-16 14:06:35 +0900

    feat: ActivityPubの通信に使うHTTPクライアントのUserAgentを設定

--- src/main/kotlin/dev/usbharu/hideout/application/config/HttpClientConfig.kt
+++ src/main/kotlin/dev/usbharu/hideout/application/config/HttpClientConfig.kt
@@ -2,15 +2,18 @@ package dev.usbharu.hideout.application.config
 
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.cache.*
 import io.ktor.client.plugins.logging.*
+import org.springframework.boot.info.BuildProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class HttpClientConfig {
     @Bean
-    fun httpClient(): HttpClient = HttpClient(CIO).config {
+    fun httpClient(buildProperties: BuildProperties, applicationConfig: ApplicationConfig): HttpClient =
+        HttpClient(CIO).config {
         install(Logging) {
             logger = Logger.DEFAULT
             level = LogLevel.ALL
@@ -18,5 +21,8 @@ class HttpClientConfig {
         install(HttpCache) {
         }
         expectSuccess = true
+            install(UserAgent) {
+                agent = "Hideout/${buildProperties.version} (${applicationConfig.url})"
+            }
     }
 }
