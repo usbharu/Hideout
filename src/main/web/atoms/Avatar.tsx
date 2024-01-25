b65f9dc120ed158b0a96572878303be4ecab0d11
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-25 15:17:09 +0900

    chore: 不要になったWeb UIのコードを削除

--- src/main/web/atoms/Avatar.tsx
+++
@@ -1,8 +0,0 @@
-import {Avatar as SuidAvatar} from "@suid/material";
-import {Component, JSXElement} from "solid-js";
-
-export const Avatar: Component<{ src: string }> = (props): JSXElement => {
-    return (
-        <SuidAvatar src={props.src}/>
-    )
-}
