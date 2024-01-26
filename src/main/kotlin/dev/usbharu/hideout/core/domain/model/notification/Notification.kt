5b523fccc9f1f57709dd73269d4175455338f48e
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-26 21:08:37 +0900

    feat: 通知のインターフェイスを作成

---
+++ src/main/kotlin/dev/usbharu/hideout/core/domain/model/notification/Notification.kt
@@ -0,0 +1,12 @@
+package dev.usbharu.hideout.core.domain.model.notification
+
+import java.time.Instant
+
+data class Notification(
+    val userId: Long,
+    val sourceActorId: Long?,
+    val postId: Long?,
+    val text: String?,
+    val reactionId: Long?,
+    val createdAt: Instant
+)
