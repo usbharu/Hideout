864440877a334a33ff16d8ad4881a8e8a9594482
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-26 21:09:51 +0900

    feat: Notificationにidを追加

--- src/main/kotlin/dev/usbharu/hideout/core/domain/model/notification/Notification.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/domain/model/notification/Notification.kt
@@ -3,6 +3,7 @@ package dev.usbharu.hideout.core.domain.model.notification
 import java.time.Instant
 
 data class Notification(
+    val id: Long,
     val userId: Long,
     val sourceActorId: Long?,
     val postId: Long?,
