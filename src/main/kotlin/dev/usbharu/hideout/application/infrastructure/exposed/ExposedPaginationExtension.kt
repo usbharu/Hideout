69c8a18bd1a21181cf7ac5300b9b0854cc0fa89c
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-29 18:03:48 +0900

    refactor: Kotlin/JVMの制限のための変更

--- src/main/kotlin/dev/usbharu/hideout/application/infrastructure/exposed/ExposedPaginationExtension.kt
+++ src/main/kotlin/dev/usbharu/hideout/application/infrastructure/exposed/ExposedPaginationExtension.kt
@@ -1,25 +1,11 @@
 package dev.usbharu.hideout.application.infrastructure.exposed
 
-import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.ExpressionWithColumnType
 import org.jetbrains.exposed.sql.Query
 import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.andWhere
 
-fun Query.pagination(page: Page, exp: ExpressionWithColumnType<Long>): Query {
-    if (page.minId != null) {
-        page.maxId?.let { andWhere { exp.less(it) } }
-        page.minId?.let { andWhere { exp.greater(it) } }
-    } else {
-        page.maxId?.let { andWhere { exp.less(it) } }
-        page.sinceId?.let { andWhere { exp.greater(it) } }
-        this.orderBy(exp, SortOrder.DESC)
-    }
-    page.limit?.let { limit(it) }
-    return this
-}
-
-fun Query.pagination(page: Page, exp: ExpressionWithColumnType<EntityID<Long>>): Query {
+fun <S> Query.pagination(page: Page, exp: ExpressionWithColumnType<S>): Query {
     if (page.minId != null) {
         page.maxId?.let { andWhere { exp.less(it) } }
         page.minId?.let { andWhere { exp.greater(it) } }
