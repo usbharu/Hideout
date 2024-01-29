cd12a2b527824212d0e73a75ef9587a6319c5133
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-29 16:10:17 +0900

    feat: ページネーションの統一化

---
+++ src/main/kotlin/dev/usbharu/hideout/application/infrastructure/exposed/Page.kt
@@ -0,0 +1,36 @@
+package dev.usbharu.hideout.application.infrastructure.exposed
+
+sealed class Page {
+    abstract val maxId: Long?
+    abstract val sinceId: Long?
+    abstract val minId: Long?
+    abstract val limit: Int?
+
+    data class PageByMaxId(
+        override val maxId: Long?,
+        override val sinceId: Long?,
+        override val limit: Int?
+    ) : Page() {
+        override val minId: Long? = null
+    }
+
+    data class PageByMinId(
+        override val maxId: Long?,
+        override val minId: Long?,
+        override val limit: Int?
+    ) : Page() {
+        override val sinceId: Long? = null
+    }
+
+    companion object {
+        fun of(maxId: Long?, sinceId: Long?, minId: Long?, limit: Int?): Page = if (minId != null) {
+            PageByMinId(
+                maxId, minId, limit
+            )
+        } else {
+            PageByMaxId(
+                maxId, sinceId, limit
+            )
+        }
+    }
+}
