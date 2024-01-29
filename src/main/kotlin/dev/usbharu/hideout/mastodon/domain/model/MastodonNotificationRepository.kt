1612dcc905ba6f0c5273a630fdbf67e116994268
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-29 19:37:30 +0900

    refactor: Notification APIをPagination APIに対応

--- src/main/kotlin/dev/usbharu/hideout/mastodon/domain/model/MastodonNotificationRepository.kt
+++ src/main/kotlin/dev/usbharu/hideout/mastodon/domain/model/MastodonNotificationRepository.kt
@@ -1,5 +1,8 @@
 package dev.usbharu.hideout.mastodon.domain.model
 
+import dev.usbharu.hideout.application.infrastructure.exposed.Page
+import dev.usbharu.hideout.application.infrastructure.exposed.PaginationList
+
 interface MastodonNotificationRepository {
     suspend fun save(mastodonNotification: MastodonNotification): MastodonNotification
     suspend fun deleteById(id: Long)
@@ -15,6 +18,13 @@ interface MastodonNotificationRepository {
         typesTmp: MutableList<NotificationType>,
         accountId: List<Long>
     ): List<MastodonNotification>
+
+    suspend fun findByUserIdAndInTypesAndInSourceActorId(
+        loginUser: Long,
+        types: List<NotificationType>,
+        accountId: List<Long>,
+        page: Page
+    ): PaginationList<MastodonNotification, Long>
 
     suspend fun deleteByUserId(userId: Long)
     suspend fun deleteByUserIdAndId(userId: Long, id: Long)
--- src/main/kotlin/dev/usbharu/hideout/mastodon/service/notification/NotificationApiService.kt
+++ src/main/kotlin/dev/usbharu/hideout/mastodon/service/notification/NotificationApiService.kt
@@ -1,5 +1,7 @@
 package dev.usbharu.hideout.mastodon.service.notification
 
+import dev.usbharu.hideout.application.infrastructure.exposed.Page
+import dev.usbharu.hideout.application.infrastructure.exposed.PaginationList
 import dev.usbharu.hideout.domain.mastodon.model.generated.Notification
 import dev.usbharu.hideout.mastodon.domain.model.NotificationType
 
@@ -15,6 +17,14 @@ interface NotificationApiService {
         excludeTypes: List<NotificationType>,
         accountId: List<Long>
     ): List<Notification>
+
+    suspend fun notifications(
+        loginUser: Long,
+        types: List<NotificationType>,
+        excludeTypes: List<NotificationType>,
+        accountId: List<Long>,
+        page: Page
+    ): PaginationList<Notification, Long>
 
     suspend fun fingById(loginUser: Long, notificationId: Long): Notification?
 
