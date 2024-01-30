e028da59257222b7f065485aec90bc62d62b5946
usbharu <64310155+usbharu@users.noreply.github.com>
2024-01-30 22:26:27 +0900

    feat: 修正した実装を追加

--- src/main/kotlin/dev/usbharu/hideout/application/infrastructure/exposed/ExposedPaginationExtension.kt
+++ src/main/kotlin/dev/usbharu/hideout/application/infrastructure/exposed/ExposedPaginationExtension.kt
@@ -1,9 +1,6 @@
 package dev.usbharu.hideout.application.infrastructure.exposed
 
-import org.jetbrains.exposed.sql.ExpressionWithColumnType
-import org.jetbrains.exposed.sql.Query
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.andWhere
+import org.jetbrains.exposed.sql.*
 
 fun <S> Query.pagination(page: Page, exp: ExpressionWithColumnType<S>): Query {
     if (page.minId != null) {
@@ -16,4 +13,20 @@ fun <S> Query.pagination(page: Page, exp: ExpressionWithColumnType<S>): Query {
     }
     page.limit?.let { limit(it) }
     return this
+}
+
+fun <S> Query.withPagination(page: Page, exp: ExpressionWithColumnType<S>): PaginationList<ResultRow, S> {
+    page.limit?.let { limit(it) }
+    val resultRows = if (page.minId != null) {
+        page.maxId?.let { andWhere { exp.less(it) } }
+        page.minId?.let { andWhere { exp.greater(it) } }
+        reversed()
+    } else {
+        page.maxId?.let { andWhere { exp.less(it) } }
+        page.sinceId?.let { andWhere { exp.greater(it) } }
+        orderBy(exp, SortOrder.DESC)
+        toList()
+    }
+
+    return PaginationList(resultRows, resultRows.firstOrNull()?.getOrNull(exp), resultRows.lastOrNull()?.getOrNull(exp))
 }
