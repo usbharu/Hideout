f0e2c49bb7ee8d6041941fe8bd739e3a81c9ce0f
usbharu <64310155+usbharu@users.noreply.github.com>
2024-02-03 16:20:07 +0900

    feat: equalsとtoStringを実装

--- src/main/kotlin/dev/usbharu/hideout/activitypub/domain/model/Announce.kt
+++ src/main/kotlin/dev/usbharu/hideout/activitypub/domain/model/Announce.kt
@@ -17,4 +17,44 @@ open class Announce @JsonCreator constructor(
     type = add(type, "Announce")
 ),
     HasActor,
-    HasId
+    HasId{
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        if (!super.equals(other)) return false
+
+        other as Announce
+
+        if (apObject != other.apObject) return false
+        if (actor != other.actor) return false
+        if (id != other.id) return false
+        if (published != other.published) return false
+        if (to != other.to) return false
+        if (cc != other.cc) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = super.hashCode()
+        result = 31 * result + apObject.hashCode()
+        result = 31 * result + actor.hashCode()
+        result = 31 * result + id.hashCode()
+        result = 31 * result + published.hashCode()
+        result = 31 * result + to.hashCode()
+        result = 31 * result + cc.hashCode()
+        return result
+    }
+
+    override fun toString(): String {
+        return "Announce(" +
+                "apObject='$apObject', " +
+                "actor='$actor', " +
+                "id='$id', " +
+                "published='$published', " +
+                "to=$to, " +
+                "cc=$cc" +
+                ")" +
+                " ${super.toString()}"
+    }
+}
