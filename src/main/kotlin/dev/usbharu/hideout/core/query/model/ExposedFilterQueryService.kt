41d1e59322f0377c91052932d2ee414d349abeab
usbharu <64310155+usbharu@users.noreply.github.com>
2024-02-12 12:36:04 +0900

    feat: 未実装箇所を実装

--- src/main/kotlin/dev/usbharu/hideout/core/query/model/ExposedFilterQueryService.kt
+++ src/main/kotlin/dev/usbharu/hideout/core/query/model/ExposedFilterQueryService.kt
@@ -6,6 +6,7 @@ import dev.usbharu.hideout.core.infrastructure.exposedrepository.Filters
 import dev.usbharu.hideout.core.infrastructure.exposedrepository.toFilter
 import dev.usbharu.hideout.core.infrastructure.exposedrepository.toFilterKeyword
 import org.jetbrains.exposed.sql.Query
+import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.selectAll
 import org.springframework.stereotype.Repository
 
@@ -17,6 +18,32 @@ class ExposedFilterQueryService : FilterQueryService {
             .selectAll()
             .where { Filters.userId eq userId }
             .toFilterQueryModel()
+    }
+
+    override suspend fun findByUserId(userId: Long): List<FilterQueryModel> {
+        return Filters
+            .rightJoin(FilterKeywords)
+            .selectAll()
+            .where { Filters.userId eq userId }
+            .toFilterQueryModel()
+    }
+
+    override suspend fun findByUserIdAndId(userId: Long, id: Long): FilterQueryModel? {
+        return Filters
+            .leftJoin(FilterKeywords)
+            .selectAll()
+            .where { Filters.userId eq userId and (Filters.id eq id) }
+            .toFilterQueryModel()
+            .firstOrNull()
+    }
+
+    override suspend fun findByUserIdAndKeywordId(userId: Long, keywordId: Long): FilterQueryModel? {
+        return Filters
+            .leftJoin(FilterKeywords)
+            .selectAll()
+            .where { Filters.userId eq userId and (FilterKeywords.id eq keywordId) }
+            .toFilterQueryModel()
+            .firstOrNull()
     }
 
     private fun Query.toFilterQueryModel(): List<FilterQueryModel> {
