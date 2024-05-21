56389c73aab3141f88cd17f97ec9e11729e62f12
usbharu <64310155+usbharu@users.noreply.github.com>
2024-05-21 22:41:18 +0900

    chore: Kotlin 2.0に対応

--- owl/owl-broker/build.gradle.kts
+++ owl/owl-broker/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
-    alias(libs.plugins.kotlin.jvm)
+//    alias(libs.plugins.kotlin.jvm)
+    kotlin("jvm")
     id("com.google.protobuf") version "0.9.4"
     id("com.google.devtools.ksp") version "1.9.24-1.0.20"
 }
