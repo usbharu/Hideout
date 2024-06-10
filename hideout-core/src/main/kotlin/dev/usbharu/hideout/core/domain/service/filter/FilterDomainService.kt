586bb294d7a4ba05eb4149b0395a5ec0046cf3da
usbharu <i@usbharu.dev>
2024-06-10 14:37:16 +0900

    feat: フィルターの実装を追加

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/service/filter/FilterDomainService.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/service/filter/FilterDomainService.kt
@@ -2,21 +2,68 @@ package dev.usbharu.hideout.core.domain.service.filter
 
 import dev.usbharu.hideout.core.domain.model.filter.Filter
 import dev.usbharu.hideout.core.domain.model.filter.FilterContext
+import dev.usbharu.hideout.core.domain.model.filter.FilterResult
 import dev.usbharu.hideout.core.domain.model.filter.FilteredPost
 import dev.usbharu.hideout.core.domain.model.post.Post
+import org.springframework.stereotype.Service
 
 interface IFilterDomainService {
     fun apply(post: Post, context: FilterContext, filters: List<Filter>): FilteredPost
     fun applyAll(postList: List<Post>, context: FilterContext, filters: List<Filter>): List<FilteredPost>
 }
 
+@Service
 class FilterDomainService : IFilterDomainService {
     override fun apply(post: Post, context: FilterContext, filters: List<Filter>): FilteredPost {
-        filters.filter { it.filterContext.contains(context) }
+        val filterResults = filters
+            .filter { it.filterContext.contains(context) }
+            .flatMap { filter ->
+                val regex = filter.compileFilter()
+                post
+                    .overview
+                    ?.overview
+                    ?.let { it1 -> regex.findAll(it1) }
+                    .orEmpty()
+                    .toList()
+                    .map { FilterResult(filter, it.value) }
+                    .plus(
+                        post
+                            .text
+                            .let { regex.findAll(it) }
+                            .toList()
+                            .map { FilterResult(filter, it.value) }
+                    )
+            }
+        return FilteredPost(post, filterResults)
     }
 
     override fun applyAll(postList: List<Post>, context: FilterContext, filters: List<Filter>): List<FilteredPost> {
-        TODO("Not yet implemented")
+        return filters
+            .filter { it.filterContext.contains(context) }
+            .map { it to it.compileFilter() }
+            .flatMap { compiledFilter ->
+                postList
+                    .map { post ->
+                        val filterResults = post
+                            .overview
+                            ?.overview
+                            ?.let { overview -> compiledFilter.second.findAll(overview) }
+                            .orEmpty()
+                            .toList()
+                            .map { FilterResult(compiledFilter.first, it.value) }
+                            .plus(
+                                post
+                                    .text
+                                    .let { compiledFilter.second.findAll(it) }
+                                    .toList()
+                                    .map { FilterResult(compiledFilter.first, it.value) }
+                            )
+
+                        post to filterResults
+                    }
+
+            }
+            .map { FilteredPost(it.first, it.second) }
     }
 
 }
