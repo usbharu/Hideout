078547d5ffd61258640c8715530164d2b6e5db70
usbharu <64310155+usbharu@users.noreply.github.com>
2024-06-13 17:23:35 +0900

    feat: フィルターの追加と削除を実装

---
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/application/filter/UserRegisterFilterApplicationService.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2024 usbharu
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.usbharu.hideout.core.application.filter
+
+import dev.usbharu.hideout.core.application.shared.AbstractApplicationService
+import dev.usbharu.hideout.core.application.shared.CommandExecutor
+import dev.usbharu.hideout.core.application.shared.Transaction
+import dev.usbharu.hideout.core.application.shared.UserDetailGettableCommandExecutor
+import dev.usbharu.hideout.core.domain.model.filter.*
+import dev.usbharu.hideout.core.domain.model.filter.FilterKeyword
+import dev.usbharu.hideout.core.domain.model.userdetails.UserDetailId
+import dev.usbharu.hideout.core.domain.shared.id.IdGenerateService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+
+@Service
+class UserRegisterFilterApplicationService(
+    private val idGenerateService: IdGenerateService,
+    private val filterRepository: FilterRepository,
+    transaction: Transaction,
+) :
+    AbstractApplicationService<RegisterFilter, Filter>(
+        transaction, logger
+    ) {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(UserRegisterFilterApplicationService::class.java)
+    }
+
+    override suspend fun internalExecute(command: RegisterFilter, executor: CommandExecutor): Filter {
+        require(executor is UserDetailGettableCommandExecutor)
+
+
+        val filter = dev.usbharu.hideout.core.domain.model.filter.Filter.create(
+            FilterId(idGenerateService.generateId()),
+            UserDetailId(executor.userDetailId),
+            FilterName(command.filterName),
+            command.filterContext,
+            command.filterAction,
+            command.filterKeywords
+                .map {
+                    FilterKeyword(
+                        FilterKeywordId(idGenerateService.generateId()),
+                        FilterKeywordKeyword(it.keyword),
+                        it.filterMode
+                    )
+                }.toSet()
+        )
+
+        filterRepository.save(filter)
+        return Filter.of(filter)
+    }
+}
--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/model/filter/FilterRepository.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/model/filter/FilterRepository.kt
@@ -3,4 +3,7 @@ package dev.usbharu.hideout.core.domain.model.filter
 interface FilterRepository {
     suspend fun save(filter: Filter): Filter
     suspend fun delete(filter: Filter)
+
+    suspend fun findByFilterKeywordId(filterKeywordId: FilterKeywordId): Filter?
+    suspend fun findByFilterId(filterId: FilterId): Filter?
 }
