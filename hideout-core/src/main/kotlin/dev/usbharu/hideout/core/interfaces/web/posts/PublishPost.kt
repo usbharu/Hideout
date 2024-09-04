57afdcdd7be79a966a3eb9f335d135d2b42c151d
usbharu <i@usbharu.dev>
2024-09-04 15:48:48 +0900

    wip

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/interfaces/web/posts/PublishPost.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/interfaces/web/posts/PublishPost.kt
@@ -1,3 +1,9 @@
 package dev.usbharu.hideout.core.interfaces.web.posts
 
-data class PublishPost(var status: String? = null, var overview: String? = null, var visibility: String = "PUBLIC")
+data class PublishPost(
+    var status: String? = null,
+    var overview: String? = null,
+    var visibility: String = "PUBLIC",
+    var replyTo: Long? = null,
+    var repost: Long? = null
+)
