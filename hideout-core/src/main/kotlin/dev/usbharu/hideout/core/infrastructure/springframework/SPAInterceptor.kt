c8a8e770216b0e27933efc7eced9e724c554cb7e
usbharu <64310155+usbharu@users.noreply.github.com>
2024-09-05 15:31:10 +0900

    feat: OGP用のクラスを追加

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/infrastructure/springframework/SPAInterceptor.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/infrastructure/springframework/SPAInterceptor.kt
@@ -1,11 +1,11 @@
 package dev.usbharu.hideout.core.infrastructure.springframework
 
+import dev.usbharu.hideout.core.interfaces.web.common.OGP
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 import org.springframework.web.servlet.ModelAndView
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder
 
 @Component
 class SPAInterceptor : HandlerInterceptor {
@@ -29,18 +29,12 @@ class SPAInterceptor : HandlerInterceptor {
         if (request.session.getAttribute("s") == "f") {
             return
         }
-        val title = modelAndView?.modelMap?.getOrDefault("title", "")
-        val url = modelAndView?.modelMap?.getOrDefault(
-            "url",
-            ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString() + '?' + request.queryString
-        )
-        val description = modelAndView?.modelMap?.getOrDefault("description", "")
-        val image = modelAndView?.modelMap?.get("image")
+
+        val ogp = modelAndView?.modelMap?.get("ogp") as? OGP
 
         modelAndView?.clear()
         modelAndView?.addObject("nsUrl", request.requestURI + "?s=f" + request.queryString?.let { "&$it" }.orEmpty())
-        modelAndView?.addAllObjects(mapOf("title" to title, "url" to url, "description" to description))
-        image?.let { modelAndView?.addObject("image", it) }
+        modelAndView?.addObject("ogp", ogp)
         modelAndView?.viewName = "index"
     }
 }
