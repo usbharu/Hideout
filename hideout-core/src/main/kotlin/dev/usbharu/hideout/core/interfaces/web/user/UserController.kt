b3a5749b54778966f9302461c6dc99539a25c4c2
usbharu <64310155+usbharu@users.noreply.github.com>
2024-09-07 18:46:49 +0900

    feat: ユーザー詳細からフォロー/アンフォローできるように

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/interfaces/web/user/UserController.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/interfaces/web/user/UserController.kt
@@ -3,16 +3,24 @@ package dev.usbharu.hideout.core.interfaces.web.user
 import dev.usbharu.hideout.core.application.actor.GetActorDetail
 import dev.usbharu.hideout.core.application.actor.GetActorDetailApplicationService
 import dev.usbharu.hideout.core.application.instance.GetLocalInstanceApplicationService
+import dev.usbharu.hideout.core.application.relationship.followrequest.FollowRequest
+import dev.usbharu.hideout.core.application.relationship.followrequest.UserFollowRequestApplicationService
+import dev.usbharu.hideout.core.application.relationship.get.GetRelationship
+import dev.usbharu.hideout.core.application.relationship.get.GetRelationshipApplicationService
+import dev.usbharu.hideout.core.application.relationship.unfollow.Unfollow
+import dev.usbharu.hideout.core.application.relationship.unfollow.UserUnfollowApplicationService
 import dev.usbharu.hideout.core.application.timeline.GetUserTimeline
 import dev.usbharu.hideout.core.application.timeline.GetUserTimelineApplicationService
 import dev.usbharu.hideout.core.domain.model.support.acct.Acct
 import dev.usbharu.hideout.core.domain.model.support.page.Page
 import dev.usbharu.hideout.core.domain.model.support.principal.Anonymous
+import dev.usbharu.hideout.core.domain.model.support.principal.LocalUser
 import dev.usbharu.hideout.core.infrastructure.springframework.SpringSecurityFormLoginPrincipalContextHolder
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
@@ -20,7 +28,11 @@ class UserController(
     private val getLocalInstanceApplicationService: GetLocalInstanceApplicationService,
     private val getUserDetailApplicationService: GetActorDetailApplicationService,
     private val springSecurityFormLoginPrincipalContextHolder: SpringSecurityFormLoginPrincipalContextHolder,
-    private val getUserTimelineApplicationService: GetUserTimelineApplicationService
+    private val getUserTimelineApplicationService: GetUserTimelineApplicationService,
+    private val userFollowRequestApplicationService: UserFollowRequestApplicationService,
+    private val getActorDetailApplicationService: GetActorDetailApplicationService,
+    private val userUnfollowApplicationService: UserUnfollowApplicationService,
+    private val userGetRelationshipApplicationService: GetRelationshipApplicationService
 ) {
     @GetMapping("/users/{name}")
     suspend fun userById(
@@ -38,6 +50,15 @@ class UserController(
             "user",
             actorDetail
         )
+        val relationship =
+            if (principal is LocalUser) {
+                userGetRelationshipApplicationService.execute(GetRelationship(actorDetail.id), principal)
+            } else {
+                null
+            }
+
+
+        model.addAttribute("relationship", relationship)
         model.addAttribute(
             "userTimeline",
             getUserTimelineApplicationService.execute(
@@ -49,5 +70,25 @@ class UserController(
             )
         )
         return "userById"
+    }
+
+    @PostMapping("/users/{name}/follow")
+    suspend fun follow(@PathVariable name: String): String {
+        val principal = springSecurityFormLoginPrincipalContextHolder.getPrincipal()
+
+        val actorDetail = getActorDetailApplicationService.execute(GetActorDetail(Acct.of(name), null), principal)
+        userFollowRequestApplicationService.execute(FollowRequest((actorDetail.id)), principal)
+
+        return "redirect:/users/$name"
+    }
+
+    @PostMapping("/users/{name}/unfollow")
+    suspend fun unfollow(@PathVariable name: String): String {
+        val principal = springSecurityFormLoginPrincipalContextHolder.getPrincipal()
+
+        val actorDetail = getActorDetailApplicationService.execute(GetActorDetail(Acct.of(name), null), principal)
+        userUnfollowApplicationService.execute(Unfollow((actorDetail.id)), principal)
+
+        return "redirect:/users/$name"
     }
 }
