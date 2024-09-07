daf676503d3d62c8c9d3585eb33da2b5089d0a57
usbharu <64310155+usbharu@users.noreply.github.com>
2024-09-08 01:10:12 +0900

    feat: ReactionRepositoryを追加

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/model/reaction/ReactionRepository.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/domain/model/reaction/ReactionRepository.kt
@@ -2,6 +2,6 @@ package dev.usbharu.hideout.core.domain.model.reaction
 
 interface ReactionRepository {
     suspend fun save(reaction: Reaction): Reaction
-    suspend fun findById(reactionId: String): Reaction?
+    suspend fun findById(reactionId: ReactionId): Reaction?
     suspend fun delete(reaction: Reaction)
 }
