1c809113651905cb92c05aa58f9af98f095e1657
usbharu <64310155+usbharu@users.noreply.github.com>
2024-09-13 13:40:52 +0900

    feat: ドメインイベントのサブスクライバー一覧を出力できるように

--- hideout-core/src/main/kotlin/dev/usbharu/hideout/core/application/domainevent/subscribers/RegisterTimelineSetTimelineStoreSubscriber.kt
+++ hideout-core/src/main/kotlin/dev/usbharu/hideout/core/application/domainevent/subscribers/RegisterTimelineSetTimelineStoreSubscriber.kt
@@ -5,17 +5,20 @@ import dev.usbharu.hideout.core.application.timeline.SetTimleineStore
 import dev.usbharu.hideout.core.domain.event.timeline.TimelineEvent
 import dev.usbharu.hideout.core.domain.event.timeline.TimelineEventBody
 import dev.usbharu.hideout.core.domain.model.support.principal.Anonymous
+import dev.usbharu.hideout.core.domain.shared.domainevent.DomainEvent
 import org.springframework.stereotype.Component
 
 @Component
 class RegisterTimelineSetTimelineStoreSubscriber(
     private val domainEventSubscriber: DomainEventSubscriber,
     private val setTimelineToTimelineStoreApplicationService: SetTimelineToTimelineStoreApplicationService
-) : Subscriber {
+) : Subscriber, DomainEventConsumer<TimelineEventBody> {
 
     override fun init() {
-        domainEventSubscriber.subscribe<TimelineEventBody>(TimelineEvent.CREATE.eventName) {
-            setTimelineToTimelineStoreApplicationService.execute(SetTimleineStore(it.body.getTimelineId()), Anonymous)
-        }
+        domainEventSubscriber.subscribe<TimelineEventBody>(TimelineEvent.CREATE.eventName, this)
+    }
+
+    override suspend fun invoke(p1: DomainEvent<TimelineEventBody>) {
+        setTimelineToTimelineStoreApplicationService.execute(SetTimleineStore(p1.body.getTimelineId()), Anonymous)
     }
 }
