5cc82671357fb777f3d0ad81fd9ab4f05ed8e587
usbharu <i@usbharu.dev>
2024-09-16 01:08:25 +0900

    test: MediaAPIのテストを復活

--- hideout-mastodon/src/test/kotlin/mastodon/media/MediaTest.kt
+++ hideout-mastodon/src/test/kotlin/mastodon/media/MediaTest.kt
@@ -40,7 +40,7 @@ import org.springframework.web.context.WebApplicationContext
 @SpringBootTest(classes = [SpringApplication::class])
 @AutoConfigureMockMvc
 @Transactional
-@Sql("/sql/actors.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+@Sql("/sql/actors.sql","/sql/userdetail.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 class MediaTest {
 
     @Autowired
