ae0e4a40132e4373a707177a8a5d1607e3b04ab2
usbharu <i@usbharu.dev>
2025-02-14 17:45:13 +0900

    fix: Spring Securityが正常に動作していなかったので修正

--- hideout/hideout-activitypub/src/main/kotlin/dev/usbharu/hideout/activitypub/config/ActivityPubSecurityConfig.kt
+++ hideout/hideout-activitypub/src/main/kotlin/dev/usbharu/hideout/activitypub/config/ActivityPubSecurityConfig.kt
@@ -8,6 +8,8 @@ import org.springframework.http.HttpMethod.POST
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.util.matcher.AnyRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 
 @Configuration
 class ActivityPubSecurityConfig {
@@ -15,11 +17,16 @@ class ActivityPubSecurityConfig {
     @Order(4)
     fun activityPubSecurityFilterChain(http: HttpSecurity): SecurityFilterChain {
         http {
+            securityMatcher(RequestMatcher {
+                val accept = it.getHeader("Accept") ?: ""
+                return@RequestMatcher accept == "application/json" || accept == "application/activity+json"
+            })
             authorizeHttpRequests {
                 authorize(POST, "/inbox", permitAll)
                 authorize(POST, "/users/{username}/inbox", permitAll)
                 authorize(GET, "/outbox", permitAll)
                 authorize(GET, "/users/{username}/outbox", permitAll)
+                authorize(GET, "/users/{username}", permitAll)
             }
         }
         return http.build()
