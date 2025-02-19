17cc92d3449b7cce886caf57a03152ccfe115e29
usbharu <i@usbharu.dev>
2025-02-19 12:43:40 +0900

    test: テストを追加

---
+++ hideout/hideout-activitypub/src/test/kotlin/wellknown/WebFingerControllerTest.kt
@@ -0,0 +1,55 @@
+package wellknown
+
+import dev.usbharu.hideout.SpringApplication
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity
+import org.springframework.test.context.jdbc.Sql
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.web.context.WebApplicationContext
+
+@SpringBootTest(classes = [SpringApplication::class])
+@AutoConfigureMockMvc
+@Transactional
+@Sql("/sql/actors.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
+class WebFingerControllerTest {
+    @Autowired
+    private lateinit var context: WebApplicationContext
+
+    private lateinit var mockMvc: MockMvc
+
+    @BeforeEach
+    fun setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+            .apply<DefaultMockMvcBuilder>(springSecurity())
+            .build()
+    }
+
+    @Test
+    fun webfinger() {
+        mockMvc
+            .get("/.well-known/webfinger?resource=acct:test@example.com") {
+                accept(MediaType.APPLICATION_JSON)
+            }
+            .asyncDispatch()
+            .andDo { print() }
+            .andExpect { status { isOk() } }
+    }
+
+    @Test
+    fun `webfinger resourceが無いと400`() {
+        mockMvc
+            .get("/.well-known/webfinger") {
+                accept(MediaType.APPLICATION_JSON)
+            }
+            .andExpect { status { isBadRequest() } }
+    }
+}
