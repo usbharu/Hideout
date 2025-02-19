ecdbf74e574a33f7b995fe593e681f5bfe0f29a2
usbharu <i@usbharu.dev>
2025-02-19 12:33:17 +0900

    refactor: WebHostMetadataをXRDに変更

--- hideout/hideout-activitypub/src/main/kotlin/dev/usbharu/hideout/activitypub/config/WebFingerHostMetaLinkConfiguration.kt
+++ hideout/hideout-activitypub/src/main/kotlin/dev/usbharu/hideout/activitypub/config/WebFingerHostMetaLinkConfiguration.kt
@@ -1,6 +1,6 @@
 package dev.usbharu.hideout.activitypub.config
 
-import dev.usbharu.hideout.activitypub.application.hostmeta.Link
+import dev.usbharu.hideout.activitypub.interfaces.wellknown.Link
 import dev.usbharu.hideout.core.config.ApplicationConfig
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,7 +12,8 @@ class WebFingerHostMetaLinkConfiguration(private val applicationConfig: Applicat
         return Link(
             rel = "lrdd",
             type = "application/jrd+json",
-            template = applicationConfig.url.resolve(".well-known/webfinger").toString() + "?resource={uri}"
+            template = applicationConfig.url.resolve(".well-known/webfinger").toString() + "?resource={uri}",
+            href = null
         )
     }
 }
