98385c6b823a405d37f4bb8986797611d4bd9771
usbharu <i@usbharu.dev>
2025-05-13 14:54:40 +0900

    Revert "wip"
    This reverts commit e28b8f0d6c7ca1bffcce28ee3de2e70cd54ca74a.

--- hideout/build.gradle.kts
+++ hideout/build.gradle.kts
@@ -57,28 +57,7 @@ subprojects {
     kotlin {
         jvmToolchain(21)
     }
-
 }
-
-val mergeChildResources by tasks.registering(Copy::class) {
-    // 各子プロジェクトの resources を処理後に取得
-    dependsOn(subprojects.map { it.tasks.named("processResources") })
-
-    subprojects.forEach { sub ->
-        // 各サブプロジェクトの 'bootBuildInfo' タスクを待機するように設定
-        dependsOn(sub.tasks.named("bootBuildInfo"))
-
-        // サブプロジェクトの 'resources/main' をマージ
-        from(sub.layout.buildDirectory.dir("resources/main"))
-    }
-    into(layout.buildDirectory.dir("resources/main"))
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-}
-
-tasks.named<ProcessResources>("processResources") {
-    dependsOn(mergeChildResources)
-}
-
 
 tasks {
     register("run") {
@@ -104,7 +83,7 @@ tasks {
     }
     named<BootJar>("bootJar") {
         layered {
-            enabled = false
+            enabled.set(false)
         }
     }
 }
